246bf822fb5af5f60f2a9940ab081d8b64c0c55e
Xin-Qiu Yao <xinqiu.yao@gmail.com>
2013-07-05 16:24:56 -0400

    improve atom.select to take intersection of string and components; new function, combine.sel, to combine two atom selections via various logical operations

--- ver_devel/bio3d/R/atom.select.R
+++ ver_devel/bio3d/R/atom.select.R
@@ -3,7 +3,11 @@ function(pdb, string=NULL,
          chain=NULL, resno=NULL, resid=NULL,
          eleno=NULL, elety=NULL,
          verbose=TRUE, rm.insert=FALSE) {
-
+  ## Version 0.6 ... Wed Jul  3 12:25:18 EDT 2013
+  ##
+  ##  Include the function to take the intersection
+  ##  of string and component (Xinqiu)
+  ##
   ## Version 0.5 ... Fri Nov 30 13:10:24 EST 2012
   ##
   ##  Re-worte large parts of the keyword matching
@@ -75,6 +79,166 @@ function(pdb, string=NULL,
     ##- Splitting function for characters - split on coma & remove white space
     return( gsub(" ","", unlist(strsplit(type.sel.txt, split=",")) ) )
   }
+  
+  ##-- Parse string and return the selection
+  parse.string <- function(pdb, string, verbose, rm.insert) {
+
+     ##-- We have input selection string
+     aa <- unique(pdb$atom[,"resid"])
+     prot.aa <- c("ALA", "CYS", "ASP", "GLU", 
+                  "PHE", "GLY", "HIS", "ILE", "LYS", "LEU", "MET", "ASN", 
+                  "PRO", "GLN", "ARG", "SER", "THR", "VAL", "TRP", "TYR", 
+                  "SEP", "TPO", "MLY", "MSE", "IAS", "ABA", "CSO", "CSD", 
+                  "CYM", "CME", "CSX", "CMT", "HIE", "HIP", "HID", "HSD", 
+                  "HSE", "HSP", "DDE", "MHO", "ASX")
+     
+     hoh <- c("H2O", "OH2", "HOH", "HHO", "OHH", "SOL", "WAT",
+              "TIP", "TIP2", "TIP3", "TIP4")
+
+     not.prot.aa <- (aa[!aa %in% prot.aa])
+     ligand.aa <- not.prot.aa[!not.prot.aa %in% hoh]
+     if(length(not.prot.aa) == 0) { not.prot.aa = "xxxxx" }
+     if(length(ligand.aa) == 0) { ligand.aa = "xxxxx" }
+     
+     if (string=="h") {
+       h.atom <- which( substr(pdb$atom[,"elety"], 1, 1) %in% "H" )
+       match <- list(atom=h.atom, xyz=atom2xyz(h.atom))
+       class(match) <- "select"
+       if(verbose) 
+         cat(paste(" *  Selected", length(h.atom), "hydrogen atoms *\n"))
+       return(match)
+     }
+     if (string=="noh") {
+       noh.atom <- which( !substr(pdb$atom[,"elety"], 1, 1) %in% "H" )
+       match <- list(atom=noh.atom, xyz=atom2xyz(noh.atom))
+       class(match) <- "select"
+       if(verbose) 
+         cat(paste(" *  Selected", length(noh.atom), "non-hydrogen atoms *\n"))
+       return(match)
+     }
+     
+     ##-- Check for string 'shortcuts'
+     i <- switch(string,
+                 calpha = "//////CA/",
+                 cbeta = "//////N,CA,C,O,CB/",
+                 backbone = "//////N,CA,C,O/",
+                 back = "//////N,CA,C,O/",
+                 all = "///////",
+                 protein = paste("////",paste(prot.aa, collapse=","), "///",sep=""),
+                 notprotein = paste("////", paste(not.prot.aa, collapse=","), "///",sep=""),
+                 ligand = paste("////", paste(ligand.aa, collapse=","), "///",sep=""),
+                 water = paste("////",paste(hoh, collapse=","), "///",sep=""),
+                 notwater = paste("////", paste(aa[!aa %in% hoh], collapse=","), "///",sep=""),
+                 #noh = "!H*",
+                 #h = "H*",
+                 NA)
+     
+     if(is.na(i)) {
+       ##- No string shortcut match
+       
+       if(!substr(string,1,1)=="/") {
+         ## Check if we have a valid selection sting
+         stop("Not a valid selection string shortcut.\n\t Please use one of:
+        'calpha' 'cbeta' 'backbone'
+        'protein' 'notprotein' 'ligand'
+        'water' 'notwater'
+        'h' 'noh'\n
+       Or valid selection string:
+         /segid/chain/resno/resid/eleno/elety/ \n")
+       }
+
+     } else {
+       ##- Use string shortcut from switch function
+       if(verbose)
+         cat(paste("\n Using selection 'string' keyword shortcut:",string, "=", i, "\n\n"))
+       string = i
+     }
+     
+     sel <- unlist(strsplit(string, split = "/"))
+   
+     if (sel[1] == "") { # full selection string (starts with "/")
+       sel <- sel[-1]
+       if(length(sel) != 6) {
+         print("missing elements, should be:\n/segid/chain/resno/resid/eleno/elety/")
+       }
+   
+       names(sel) <- c("segid","chain","resno","resid","eleno","elety")
+       ##print(sel)
+   
+       blank <- rep(TRUE, nrow(pdb$atom) )
+       sel.inds <- NULL
+   
+       ## SEGID
+       if(sel["segid"] != "") { 
+         sel.inds <- cbind(sel.inds,
+                           segid=is.element( pdb$atom[,"segid"],
+                             sel.txt2type( sel["segid"] )) )
+       } else {  sel.inds <- cbind(sel.inds, segid=blank)  }
+   
+       ## CHAIN
+       if(sel["chain"] != "") {
+         sel.inds <- cbind(sel.inds,
+                           chain=is.element( pdb$atom[,"chain"],
+                             sel.txt2type( sel["chain"] )) )        
+       } else { sel.inds <- cbind(sel.inds, chain=blank)  }
+     
+       ## RESNO
+       if(sel["resno"] != "") {        
+         rn <- sel.txt2nums( sel["resno"] )
+         if(is.numeric(rn) & length(rn)==0) {
+           ## check for R object 
+           rn <- get(gsub(" ","",sel["resno"]))    
+         }       
+         sel.inds <- cbind(sel.inds,
+                           resno=is.element( as.numeric(pdb$atom[,"resno"]),
+                             rn))
+       } else {  sel.inds <- cbind(sel.inds, resno=blank)  }
+   
+       ## RESID
+       if(sel["resid"] != "") {
+         sel.inds <- cbind(sel.inds,
+                           resid=is.element(pdb$atom[,"resid"],
+                             sel.txt2type( sel["resid"] )) )
+       } else {  sel.inds <- cbind(sel.inds, resid=blank)  }
+   
+       ## ELENO
+       if(sel["eleno"] != "") {
+         sel.inds <- cbind(sel.inds,
+                           eleno=is.element(as.numeric(pdb$atom[,"eleno"]),
+                             sel.txt2nums( sel["eleno"] )) )
+       } else {  sel.inds <- cbind(sel.inds, eleno=blank)  }
+   
+       ## ELETY
+       if(sel["elety"] != "") {
+         sel.inds <- cbind(sel.inds,
+                           elety=is.element(pdb$atom[,"elety"],
+                             sel.txt2type( sel["elety"] )) )
+       } else {  sel.inds <- cbind(sel.inds, elety=blank)  }
+   
+       match.inds <- ( (apply(sel.inds, 1, sum, na.rm=TRUE)==6) )
+       ## In future, could take the inverse here for NOT selection
+       
+       if (rm.insert) { # ignore INSERT records
+         insert <- which(!is.na(pdb$atom[,"insert"]))
+         match.inds[insert] <- FALSE
+       }
+       ## return XYZ indices
+       xyz.inds <- matrix(1:length( pdb$atom[,c("x","y","z")] ),nrow=3,byrow=FALSE)
+       xyz.inds <- as.vector(xyz.inds[,match.inds])
+   
+       if (verbose) {
+         sel <- rbind( sel, apply(sel.inds, 2, sum, na.rm=TRUE) )
+         rownames(sel)=c("Stest","Natom"); print(sel)
+         cat(paste(" *  Selected a total of:",sum(match.inds),
+                   "intersecting atoms  *"),sep="\n")
+       }
+         
+       match <- list(atom=which(match.inds), xyz=xyz.inds)
+       class(match) <- "select"
+       return(match)
+     }
+  } #END parse.string
+
 
   ##-- Check on input
   if (missing(pdb)) {
@@ -88,187 +252,47 @@ function(pdb, string=NULL,
 
   ## No selection string or component, then just print PDB summary
   if( !any(got.string,  got.component) ) {
-    print.pdb(pdb) 
-    return()
+    return( print.pdb(pdb) )
   }
 
-  ## Selection string and component, then component is ignored
-  if(all(got.string, got.component)) {
-    warning("Selection string AND selection component given: using first input string only!")
-    ## Could Change this to combine string and component here!!
-  } else {
-    if(got.component) {
+## Modified for combining string and component, (Jul 3, 2013)
+#  ## Selection string and component, then component is ignored
+#  if(all(got.string, got.component)) {
+#    warning("Selection string AND selection component given: using first input string only!")
+#    ## Could Change this to combine string and component here!!
+#  } else {
 
-      ## Build selection string from input components
-      ## /segid/chain/resno/resid/eleno/elety/
-      string <- paste("//",paste(chain, collapse=","),"/",
-                      paste(resno, collapse=","),"/",
-                      paste(resid, collapse=","),"/",
-                      paste(eleno, collapse=","),"/",
-                      paste(elety, collapse=","),"/",sep="")
-    
-      rm(chain,resno,resid,eleno,elety)
-    } else {
-
-      ##-- We have input selection string
-      aa <- unique(pdb$atom[,"resid"])
-      prot.aa <- c("ALA", "CYS", "ASP", "GLU", 
-                   "PHE", "GLY", "HIS", "ILE", "LYS", "LEU", "MET", "ASN", 
-                   "PRO", "GLN", "ARG", "SER", "THR", "VAL", "TRP", "TYR", 
-                   "SEP", "TPO", "MLY", "MSE", "IAS", "ABA", "CSO", "CSD", 
-                   "CYM", "CME", "CSX", "CMT", "HIE", "HIP", "HID", "HSD", 
-                   "HSE", "HSP", "DDE", "MHO", "ASX")
-      
-      hoh <- c("H2O", "OH2", "HOH", "HHO", "OHH", "SOL", "WAT",
-               "TIP", "TIP2", "TIP3", "TIP4")
-
-      not.prot.aa <- (aa[!aa %in% prot.aa])
-      ligand.aa <- not.prot.aa[!not.prot.aa %in% hoh]
-      if(length(not.prot.aa) == 0) { not.prot.aa = "xxxxx" }
-      if(length(ligand.aa) == 0) { ligand.aa = "xxxxx" }
-      
-      if (string=="h") {
-        h.atom <- which( substr(pdb$atom[,"elety"], 1, 1) %in% "H" )
-        match <- list(atom=h.atom, xyz=atom2xyz(h.atom))
-        class(match) <- "select"
-        if(verbose) 
-          cat(paste(" *  Selected", length(h.atom), "hydrogen atoms *\n"))
-        return(match)
-      }
-      if (string=="noh") {
-        noh.atom <- which( !substr(pdb$atom[,"elety"], 1, 1) %in% "H" )
-        match <- list(atom=noh.atom, xyz=atom2xyz(noh.atom))
-        class(match) <- "select"
-        if(verbose) 
-          cat(paste(" *  Selected", length(noh.atom), "non-hydrogen atoms *\n"))
-        return(match)
-      }
-      
-      ##-- Check for string 'shortcuts'
-      i <- switch(string,
-                  calpha = "//////CA/",
-                  cbeta = "//////N,CA,C,O,CB/",
-                  backbone = "//////N,CA,C,O/",
-                  back = "//////N,CA,C,O/",
-                  all = "///////",
-                  protein = paste("////",paste(prot.aa, collapse=","), "///",sep=""),
-                  notprotein = paste("////", paste(not.prot.aa, collapse=","), "///",sep=""),
-                  ligand = paste("////", paste(ligand.aa, collapse=","), "///",sep=""),
-                  water = paste("////",paste(hoh, collapse=","), "///",sep=""),
-                  notwater = paste("////", paste(aa[!aa %in% hoh], collapse=","), "///",sep=""),
-                  #noh = "!H*",
-                  #h = "H*",
-                  NA)
-      
-      if(is.na(i)) {
-        ##- No string shortcut match
-        
-        if(!substr(string,1,1)=="/") {
-          ## Check if we have a valid selection sting
-          stop("Not a valid selection string shortcut.\n\t Please use one of:
-         'calpha' 'cbeta' 'backbone'
-         'protein' 'notprotein' 'ligand'
-         'water' 'notwater'
-         'h' 'noh'\n
-        Or valid selection string:
-          /segid/chain/resno/resid/eleno/elety/ \n")
-        }
-
-      } else {
-        ##- Use string shortcut from switch function
-        if(verbose)
-          cat(paste("\n Using selection 'string' keyword shortcut:",string, "=", i, "\n\n"))
-        string = i
-      }
-      
-    }
-  }
-  ## - end edit
-    
   ##-- Main function  
-  sel <- unlist(strsplit(string, split = "/"))
 
-  if (sel[1] == "") { # full selection string (starts with "/")
-    sel <- sel[-1]
-    if(length(sel) != 6) {
-      print("missing elements, should be:\n/segid/chain/resno/resid/eleno/elety/")
-    }
-
-    names(sel) <- c("segid","chain","resno","resid","eleno","elety")
-    ##print(sel)
-
-    blank <- rep(TRUE, nrow(pdb$atom) )
-    sel.inds <- NULL
-
-    ## SEGID
-    if(sel["segid"] != "") { 
-      sel.inds <- cbind(sel.inds,
-                        segid=is.element( pdb$atom[,"segid"],
-                          sel.txt2type( sel["segid"] )) )
-    } else {  sel.inds <- cbind(sel.inds, segid=blank)  }
-
-    ## CHAIN
-    if(sel["chain"] != "") {
-      sel.inds <- cbind(sel.inds,
-                        chain=is.element( pdb$atom[,"chain"],
-                          sel.txt2type( sel["chain"] )) )        
-    } else { sel.inds <- cbind(sel.inds, chain=blank)  }
-  
-    ## RESNO
-    if(sel["resno"] != "") {        
-      rn <- sel.txt2nums( sel["resno"] )
-      if(is.numeric(rn) & length(rn)==0) {
-        ## check for R object 
-        rn <- get(gsub(" ","",sel["resno"]))    
-      }       
-      sel.inds <- cbind(sel.inds,
-                        resno=is.element( as.numeric(pdb$atom[,"resno"]),
-                          rn))
-    } else {  sel.inds <- cbind(sel.inds, resno=blank)  }
-
-    ## RESID
-    if(sel["resid"] != "") {
-      sel.inds <- cbind(sel.inds,
-                        resid=is.element(pdb$atom[,"resid"],
-                          sel.txt2type( sel["resid"] )) )
-    } else {  sel.inds <- cbind(sel.inds, resid=blank)  }
-
-    ## ELENO
-    if(sel["eleno"] != "") {
-      sel.inds <- cbind(sel.inds,
-                        eleno=is.element(as.numeric(pdb$atom[,"eleno"]),
-                          sel.txt2nums( sel["eleno"] )) )
-    } else {  sel.inds <- cbind(sel.inds, eleno=blank)  }
-
-    ## ELETY
-    if(sel["elety"] != "") {
-      sel.inds <- cbind(sel.inds,
-                        elety=is.element(pdb$atom[,"elety"],
-                          sel.txt2type( sel["elety"] )) )
-    } else {  sel.inds <- cbind(sel.inds, elety=blank)  }
-
-    match.inds <- ( (apply(sel.inds, 1, sum, na.rm=TRUE)==6) )
-    ## In future, could take the inverse here for NOT selection
-    
-    if (rm.insert) { # ignore INSERT records
-      insert <- which(!is.na(pdb$atom[,"insert"]))
-      match.inds[insert] <- FALSE
-    }
-    ## return XYZ indices
-    xyz.inds <- matrix(1:length( pdb$atom[,c("x","y","z")] ),nrow=3,byrow=FALSE)
-    xyz.inds <- as.vector(xyz.inds[,match.inds])
-
-    if (verbose) {
-      sel <- rbind( sel, apply(sel.inds, 2, sum, na.rm=TRUE) )
-      rownames(sel)=c("Stest","Natom"); print(sel)
-      cat(paste(" *  Selected a total of:",sum(match.inds),
-                "intersecting atoms  *"),sep="\n")
-    }
-      
-    match <- list(atom=which(match.inds), xyz=xyz.inds)
-    class(match) <- "select"
-    return(match)
+  sel1 <- NULL
+  sel2 <- NULL
+  if(got.string) {
+    if(verbose) 
+       cat("\nBuild selection from input string\n\n")
+    sel1 <- parse.string(pdb, string, verbose, rm.insert)
   }
 
+  if(got.component) {
+
+    ## Build selection string from input components
+    ## /segid/chain/resno/resid/eleno/elety/
+    string <- paste("//",paste(chain, collapse=","),"/",
+                    paste(resno, collapse=","),"/",
+                    paste(resid, collapse=","),"/",
+                    paste(eleno, collapse=","),"/",
+                    paste(elety, collapse=","),"/",sep="")
+    rm(chain,resno,resid,eleno,elety)
+    if(verbose) 
+       cat("\nBuild selection from input components\n\n")
+    sel2 <- parse.string(pdb, string, verbose, rm.insert)
+  }
+
+  if(!is.null(sel1) && !is.null(sel2))
+     if(verbose)
+        cat("\nCombine selections from input string and components\n\n")
+
+  match <- combine.sel(sel1, sel2, op="AND")
+  
+  return(match)
 }
 
---
+++ ver_devel/bio3d/R/combine.sel.R
@@ -0,0 +1,33 @@
+combine.sel <- 
+ function(sel1=NULL, sel2=NULL, op="AND") {
+   if(is.null(c(sel1, sel2))) {
+      stop("Invalid selection")
+   }
+   if(is.null(sel1))
+      return(sel2)
+   if(is.null(sel2))
+      return(sel1)
+ 
+   op.tbl <- c(rep("AND",3), rep("OR",4), rep("NOT",4))
+   op <- op.tbl[match(op, c("AND","and","&","OR","or","|","+","NOT","not","!","-"))]
+   sel <- switch(op, 
+      "AND"= {
+         cat("Intersection of sel1 and sel2\n")
+         list(atom = intersect(sel1$atom, sel2$atom), 
+              xyz = intersect(sel1$xyz, sel2$xyz))
+      },
+      "OR" = {
+         cat("Union of sel1 and sel2\n")
+         list(atom = sort(union(sel1$atom, sel2$atom)), 
+              xyz = sort(union(sel1$xyz, sel2$xyz)))
+      },
+      "NOT" = {
+         cat("Sel2 is subtracted from sel1\n")
+         list(atom = setdiff(sel1$atom, sel2$atom), 
+               xyz = setdiff(sel1$xyz, sel2$xyz))
+      },
+      stop("Unknown operation") )
+   cat(paste(" *  Selected a total of:", length(sel$atom), "atoms  *\n"))
+   class(sel)="select"
+   return(sel)
+}
