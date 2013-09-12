446f6701af5605e696cf05df9d76ad0639a8ec1c
Xin-Qiu Yao <xinqiu.yao@gmail.com>
2013-09-12 10:51:16 -0400

    Minor Update of aa321.R

--- ver_devel/bio3d/R/aa321.R
+++ ver_devel/bio3d/R/aa321.R
@@ -8,14 +8,14 @@ function (aa) {
            "A","C","D","E","F","G",
            "H","I","K","L","M","N","P","Q",
            "R","S","T","V","W","Y",
-           "S","T","K","M","D","C","C","C","C","C","C","C","C"
+           "S","T","K","M","D","C","C","C","C","C","C","C","C",
            "H","H","H","H","H","H","H",
            "M", "D")
   aa3 <- c("---", "---","UNK",
            "ALA", "CYS", "ASP", "GLU", "PHE", "GLY",
            "HIS", "ILE", "LYS", "LEU", "MET", "ASN", "PRO", "GLN",
            "ARG", "SER", "THR", "VAL", "TRP", "TYR",
-           "SEP", "TPO", "MLY", "MSE", "IAS", "ABA","CSO","CSD","CYM","CME","CSX","CMT","CYX"
+           "SEP", "TPO", "MLY", "MSE", "IAS", "ABA","CSO","CSD","CYM","CME","CSX","CMT","CYX",
            "HIE", "HIP", "HID", "HSD", "HSE", "HSP","DDE",
            "MHO", "ASX")
     convert <- function(x) {
