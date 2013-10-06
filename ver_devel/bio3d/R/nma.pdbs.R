15cb4315d6c8e634cfe4525b485f9d1d693cc8c5
Lars Skjærven <larsss@gmail.com>
2013-10-06 18:10:57 +0200

    nma.pdbs: a new function for NMA on a set of aligned structures

---
+++ ver_devel/bio3d/R/nma.pdbs.R
@@ -0,0 +1,129 @@
+"nma.pdbs" <- function(aln, fit=FALSE, full=FALSE, 
+                       trim.inds=NULL, outpath = "pdbs_nma/", ...) {
+
+  if(class(aln)!="3dalign")
+    stop("input 'aln' should be a list object as obtained from 'read.fasta.pdb'")
+
+  #if(!is.null(xyz)) {
+  #  if(class(xyz)=="matrix") {
+  #    if(!identical(dim(aln$xyz), dim(xyz.core)))
+  #      stop("dimension mismatch: input 'xyz' should have same dimensions as 'aln$xyz'")
+  #  }
+  #  else
+  #    stop("input 'xyz' should be of type 'matrix'")
+  #}
+  
+  if(!is.null(outpath))
+    dir.create(outpath, FALSE)
+
+  keep <- 20
+  gaps.res <- gap.inspect(aln$ali)
+  gaps.pos <- gap.inspect(aln$xyz)
+  
+  f.inds <- NULL
+  if(!is.null(trim.inds)) {
+    strip <- TRUE
+    ##f.inds$res <- intersect(gaps.res$f.inds, trim.inds)
+    f.inds$res <- trim.inds
+    f.inds$pos <- atom2xyz(f.inds$res)
+  }
+  else {
+    strip <- FALSE
+    ## indices only used for fit.xyz
+    f.inds$res <- gaps.res$f.inds
+    f.inds$pos <- gaps.pos$f.inds
+  }
+
+  if(nrow(bounds(f.inds$res))>1 && strip)
+    warning("truncated pdbs at non-terminus positions")
+  
+  if(fit) {
+    #if(!is.null(xyz))
+    #  warning("'aln$xyz' issued to re-fitting since 'fit=TRUE'")
+
+    xyz <- fit.xyz(fixed = aln$xyz[1, ], mobile = aln,
+                   fixed.inds = f.inds$pos, mobile.inds = f.inds$pos)
+                   ##pdb.path = "", pdbext = "", outpath = "core_fitlsq/", full.pdbs = TRUE, het2atom = TRUE)
+  }
+  
+  if(strip) {
+    flucts <- matrix(NA, nrow=nrow(gaps.res$bin), ncol=length(f.inds$res))
+  }
+  else {
+    flucts <- matrix(NA, nrow=nrow(gaps.res$bin), ncol=ncol(gaps.res$bin))
+  }
+
+  ## List object to store each modes object
+  all.modes <- NULL
+
+  ## 3D array- containing the modes vectors for each structure
+  modes.array <- NULL
+  
+  if(full) {
+    ## 3N x Num-modes x Num-structs
+    modes.array <- array(NA, dim=c(length(f.inds$pos), keep, nrow(gaps.res$bin)))
+    all.modes <- list()
+  }
+    
+  if(is.null(outpath))
+    fname <- tempfile(fileext = "pdb")
+
+  
+  for ( i in 1:nrow(aln$xyz) ) {
+    if(!strip) {
+      f.inds <- NULL
+      f.inds$res <- which(gaps.res$bin[i,]==0)
+      f.inds$pos <- atom2xyz(f.inds$res)
+    }
+    
+    resno <- aln$resno[i,f.inds$res]
+    chain <- aln$chain[i,f.inds$res]
+    resid <- aa123(aln$ali[i,f.inds$res])
+    
+    if(is.null(xyz))
+      tmp.xyz <- aln$xyz[i, f.inds$pos]
+    else
+      tmp.xyz <- xyz[i, f.inds$pos]
+
+    if(!is.null(outpath))
+      fname <- paste(outpath, basename(aln$id[i]), sep="")
+    
+    write.pdb(pdb=NULL, xyz=tmp.xyz, resno=resno, chain=chain,
+              resid=resid, file=fname)
+    tmp.pdb <- read.pdb(fname)
+    
+    if(is.null(outpath))
+       unlink(fname)
+    
+    modes <- nma(tmp.pdb, ...)
+
+    if(full) {
+      new.modes <- matrix(NA, ncol=keep, nrow=ncol(aln$xyz))
+      
+      j <- 1
+      for(k in 7:(keep+6)) {
+        new.modes[f.inds$pos, j] <- modes$U[,k]
+        j <- j+1
+      }
+      
+      ##print(dim(new.modes))
+      
+      all.modes[[i]] <- modes
+      if(strip)
+        modes.array[,,i] <- new.modes[f.inds$pos,]
+      else
+        modes.array[,,i] <- new.modes[gaps.pos$f.inds,]
+      
+    }
+    
+    if(!strip)
+      flucts[i, f.inds$res] <- modes$fluctuations
+    else
+      flucts[i, ] <- modes$fluctuations
+  }
+  
+  row.names(flucts) <- row.names(aln$xyz)
+  out <- list(fluctuations=flucts, modes.array=modes.array, all.modes=all.modes)
+      
+  return(out)
+}
