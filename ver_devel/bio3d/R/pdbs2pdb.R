97d8fb28a03d54541d0aa23b337568e12916d28e
Lars Skjaerven <larsss@gmail.com>
2013-10-28 16:41:05 +0100

    New function proposal: pdbs2pdb() - convert a pdbs (3dalign) object to individual pdb objects

---
+++ ver_devel/bio3d/R/pdbs2pdb.R
@@ -0,0 +1,36 @@
+"pdbs2pdb" <- function(pdbs, inds=NULL) {
+  if(class(pdbs)!="3dalign") {
+    stop("Input 'pdbs' should be of class '3dalign', e.g. from pdbaln() or read.fasta.pdb()")
+  }
+  
+  if(is.null(inds))
+    inds <- seq(1, length(pdbs$id))
+
+  ## Temporaray file
+  fname <- tempfile(fileext = "pdb")
+
+  ## Set indicies
+  gaps.res <- gap.inspect(pdbs$ali)
+  gaps.pos <- gap.inspect(pdbs$xyz)
+
+  
+  all.pdbs <- list()
+  for ( i in 1:length(inds) ) {
+    j <- inds[i]
+    
+    ## Set indices for this structure only
+    f.inds <- NULL
+    f.inds$res <- which(gaps.res$bin[j,]==0)
+    f.inds$pos <- atom2xyz(f.inds$res)
+
+    ## Make a temporary PDB object
+    write.pdb(pdb=NULL,
+              xyz  =pdbs$xyz[j,f.inds$pos],   resno=pdbs$resno[j,f.inds$res],
+              resid=pdbs$resid[j,f.inds$res], chain=pdbs$chain[j,f.inds$res],
+              file=fname)
+
+    all.pdbs[[i]] <- read.pdb(fname)
+  }
+  names(all.pdbs) <- sub(".pdb$", "", basename(pdbs$id[inds]))
+  return(all.pdbs)
+}
