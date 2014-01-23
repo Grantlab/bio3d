7598c9a4797842033d885307acc22a1c26e73667
gscara <guidoscarabelli@hotmail.com>
2014-01-23 14:56:09 -0500

    OTHER: new names for print.cna and summary.cna

--- ver_devel/bio3d/R/print.cna.R
+++ ver_devel/bio3d/R/print.cna.R
@@ -1,24 +1,20 @@
 print.cna <- function(x, ...) {
 
-  ## ToDo: !! Should also print details of x$clustered.network !!
-  
   y <- summary.cna(x, ...)
 
-  l1 <- paste( "\nRAW NODES#:", x$raw.communities$vcount,
-              "   COMMUNITIES#:", max(x$raw.communities$membership),
-              "   EDGES#:", ecount(x$raw.network))
+  l1 <- paste( "\nRAW NODES#:", x$communities$vcount,
+              "   COMMUNITIES#:", max(x$communities$membership),
+              "   EDGES#:", ecount(x$network))
 
-  l2 <- paste( "\nCLUSTERED NODES#:", x$clustered.communities$vcount,
-              "   COMMUNITIES#:", max(x$clustered.communities$membership),
-              "   EDGES#:", ecount(x$clustered.network))
+
+  l2 <- paste("   EDGES#:", ecount(x$community.network))
 
   
   i <- paste( attributes(x)$names, collapse=", ")
-  #cat(l1, l2,"\n");cat(strwrap(paste(" + attr:",i,"\n"),width=60, exdent=8), sep="\n")
   cat(strwrap(paste(" + attr:",i,"\n"),width=60, exdent=8), sep="\n")
   cat(l1,"\n")
-  print.igraph(x$raw.network)
+  print.igraph(x$network)
   cat(l2,"\n")
-  print.igraph(x$clustered.network)
+  print.igraph(x$community.network)
 
 }
--- ver_devel/bio3d/R/summary.cna.R
+++ ver_devel/bio3d/R/summary.cna.R
@@ -8,9 +8,9 @@ summary.cna <- function(object, ...) {
     stop("Input should be a cna network object")
   }
 
-  size <- table(object$raw.communities$membership)
+  size <- table(object$communities$membership)
   id   <- names(size)
-  memb <- sapply(id, function(i) { which(object$raw.communities$membership==i) } )
+  memb <- sapply(id, function(i) { which(object$communities$membership==i) } )
   ## NOTE: Perhaps the memb should be names() of which inds
   ##       rather than the inds themselves as it is curently?
   ##memb <- sapply(id, function(i) { names(which(object$membership==i)) } )
@@ -43,35 +43,15 @@ summary.cna <- function(object, ...) {
     members <- unlist(lapply(memb, paste, collapse=", "))
   }
   
-  ## Print to terminal
-  ###write.table(cbind(id, size, members), sep="\t",
-  ###            row.names=FALSE, quote=FALSE)
-  ## NOTE: Maybe strwrap(), sprintf() or format() could help 
-  ##       indent the second line of the above print when members
-  ##       is over a certain length?
-  ## ALSO: Might be useful to sumarize other attributes in object
-
-  ## Print with groupings (clustered membership) and ordered by member size
-  z <- cbind(Group=object$clustered.communities$members, id, size, members)
-  a <- z[order(z[,"Group"], -as.numeric(z[,"size"]), as.numeric(z[,"id"])),]
-
-  b <- bounds(a[,"Group"], dup.inds=T)[,c("start","end"), drop=FALSE]
-  a[ duplicated(a[,"Group"]),"Group"] = " "
-  
-  for(i in 1:nrow(b)) {
-    inds <- unbound(b[i,1], b[i,2] )
-    write.table(a[inds,,drop=FALSE], na=" ", sep="\t",
-                row.names=FALSE, quote=FALSE, ...)
-
-    ## Summary of groups (i.e. clustered membership)
-    cat(paste("   -- Sum: ",nrow(a[inds,,drop=FALSE]),
-              "(communities) |", sum(as.numeric(a[inds,"size",drop=FALSE])),
-              "(members) --",sep=" "),"\n\n")
-  }
+  z <- cbind(id, size, members)
 
   ## Output silently as a list
-  tbl <- data.frame( apply(z[,1:3],2,as.numeric), members=z[,4],
+  
+  tbl <- data.frame( apply(z[,1:2],2,as.numeric), members=z[,3],
                     stringsAsFactors=FALSE )
+  
   y <- list("id"=id, "size"=size, "members"=memb, "tbl"=tbl)
+  
+  return(y)
 }
 
