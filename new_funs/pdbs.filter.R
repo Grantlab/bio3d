eeabcd375f10b0a28e0a139a93eacad4f33d7b6a
Lars Skjærven <larsss@gmail.com>
2014-05-09 17:40:57 +0200

    NEW: pdbs.filter.R Proposing a function to trim a 3dalign pdbs object (e.g. either remove entire structures (rows) or columns). Useful for filtering out structures without doing a complete re-align (ie with function pdbaln). Nice in combintation with rmsd.filter to remove structurally redundant files, or inspect.connectivity to remove files with missing in-structure residues.

---
+++ new_funs/pdbs.filter.R
@@ -0,0 +1,31 @@
+## Use for trimming a 3dalign object, either by removing structures,
+## or by removing columns 
+
+pdbs.filter <- function(pdbs, row.inds=NULL, col.inds=NULL) {
+  if(!inherits(pdbs, "3dalign"))
+    stop("input 'pdbs' should be a list object as obtained from 'read.fasta.pdb'")
+  
+  ## Log the call
+  cl <- match.call()
+  
+  if(is.null(col.inds))
+    col.inds <- seq(1, ncol(pdbs$resno), by=1)
+  if(is.null(row.inds))
+    row.inds <- seq(1, nrow(pdbs$resno), by=1)
+  
+  gaps <- gap.inspect(pdbs$resno[row.inds, col.inds])
+  col.inds <- which(gaps$col < length(row.inds))
+  
+  new <- NULL
+  new$id    =pdbs$id[row.inds]
+  new$xyz   =pdbs$xyz[row.inds, atom2xyz(col.inds)]
+  new$resno =pdbs$resno[row.inds, col.inds]
+  new$b     =pdbs$b[row.inds, col.inds]
+  new$chain =pdbs$chain[row.inds, col.inds]
+  new$ali   =pdbs$ali[row.inds, col.inds]
+  new$resid =pdbs$resid[row.inds, col.inds]
+  new$call  =cl
+  
+  class(new) <- "3dalign"
+  return(new)
+}
