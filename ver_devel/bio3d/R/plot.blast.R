6e152e86c09f395820474b2785969de1d9075d0d
Barry Grant <bjgrant@umich.edu>
2014-05-12 21:23:02 -0400

    ENHANCEMENT: Improvements to plot.blast() internals
    No longer requires 'cut.seed' or 'cutoff' to be specified.
    Will still produce output and group partitioning even with large hit lists (> 2000 hits)
    User can now decide not to run clustering, and continue, or quit function with large hit lists.
    
    -- N.B. Needs testing with diverse and large blast results --

--- ver_devel/bio3d/R/plot.blast.R
+++ ver_devel/bio3d/R/plot.blast.R
@@ -1,60 +1,101 @@
 `plot.blast` <-
-function(x, cutoff=NULL, cut.seed=110, mar=c(4, 4, 1, 2), cex.lab=1.5, ...) {
+function(x, cutoff=NULL, cut.seed=NULL, cluster=TRUE, mar=c(2, 5, 1, 1), cex=1.5, ...) {
 
   ## b <- blast.pdb( pdbseq( read.pdb("4q21") ) )
-  ## plot(b, 188)
-  
-  nhit <- length(x$mlog.evalue)
-  if(nhit > 2000) {
-    continue <- readline(" More than 2000 hits, clustering may take some time, continue [y/n]:")
-    continue <- ifelse(grepl("y",continue), TRUE, FALSE)
-    if(!continue) { stop("user stop") }
+  ## plot(b, 188)  ## cut.seed=110
+
+
+  panelplot <- function(z=x$mlog.evalue, ylab="-log(Evalue)", gp=gp, ...) {
+    z=as.numeric(z)
+    plot(z, xlab="", ylab=ylab, col=gps, ...)
+    abline(v=gp, col="gray70", lty=3)
+
+    pos=c(rep(4, length(gp))[-length(gp)],2)
+    text(  gp, z[gp], 
+          labels=paste0("Nhit=",gp ,", x=", round(z[gp])), 
+          col="black", pos=pos, cex=cex, ...) ##"gray50"
   }
 
-  ##- Setup Plot alignment stats overview
+
+  ##- Setup plot arangment
   opar <- par(no.readonly = TRUE)
   on.exit(par(opar))
-  par(mfcol=c(4,1), mar=mar, cex.lab=cex.lab)
+  par(mfcol=c(4,1), mar=mar, cex.lab=cex)
 
-  
-  
-  ## Partition into groups
-  hc <- hclust( dist(x$mlog.evalue) )
-  if(!is.null(cutoff)) { cut.seed=cutoff } ## Need a better way to estimate grps
-  gps <- cutree(hc, h=cut.seed)
 
-  gp.mins <- rep(NA, max(gps))
-  for(i in 1:max(gps)) {
-    gp.mins[i] <- min(x$mlog.evalue[gps==i])
+  ##- Find the point pair with largest diff evalue
+  dx <- abs(diff(x$mlog.evalue))
+  dx.cut = which.max(dx)
+
+
+  if(!is.null(cutoff)) {
+    ##- Use suplied cutoff
+    gps = rep(2, length(x$mlog.evalue))
+    gps[ (x$mlog.evalue >= cutoff) ] = 1
+
+  } else {
+
+    if(cluster) {
+      ## Ask USER whether to continue with clustering with many hits  
+      nhit <- length(x$mlog.evalue)
+      if(nhit > 1500) {
+        cluster <- readline( paste0(" Note: ", nhit, 
+          " hits, continue with TIME-CONSUMING clustering [y/n/q](n): ") )
+
+        cluster <- switch(cluster, y=TRUE, yes=TRUE, q="QUIT", FALSE)
+        if(cluster=="QUIT") { stop("user stop") }
+      }
+    }
+
+    if(is.null(cut.seed)) {
+      ## Use mid-point of largest diff pair as seed for
+      ##  cluster grps (typical PDB values are ~110)
+      cut.seed = mean( x$mlog.evalue[dx.cut:(dx.cut+1)] )
+    }
+
+    if(cluster){
+      ##- Partition into groups via clustering 
+      ##  In future could use changepoint::cpt.var
+      hc <- hclust( dist(x$mlog.evalue) )
+      if(!is.null(cutoff)) { cut.seed=cutoff } 
+      gps <- cutree(hc, h=cut.seed)
+    } 
+
+    if(!cluster || (length(unique(gps))==1)) {
+      ##- Either we don't want to run hclust or hclust/cutree 
+      ##   has returned only one grp so here we will divide   
+      ##   into two grps at point of largest diff
+      gps = rep(2, length(x$mlog.evalue))
+      gps[1:dx.cut]=1
+    }
   }
-  gp.end <- c(which(as.logical(diff(gps))), length(gps))
-  gp.cut <- floor(gp.mins)
-  gp.num <- cumsum( table(gps) )
 
-  cat("  * Possible cutoff values include:\n\t\t", floor(gp.mins),
-      "\n    Yielding Nhits:\n\t\t", gp.num, " \n\n")
+  gp.inds <- na.omit(rle2(gps)$inds)
+  gp.nums <- x$mlog.evalue[gp.inds]
+
+  cat("  * Possible cutoff values:   ", floor(gp.nums), "\n",
+      "           Yielding Nhits:   ", gp.inds, "\n\n")
 
   if( is.null(cutoff) ) {
-    ## Pick a cutoff close to 110
-    cut.ind <- which.min(abs(gp.cut - cut.seed))
-    cutoff <- gp.cut[ cut.ind ]
-
-    cat(" ** Chosen cutoff value of:\n\t\t", cutoff,
-        "\n    Yielding Nhits:\n\t\t", gp.num[cut.ind], " \n")
-      
+    ## Pick a cutoff close to cut.seed
+    i <- which.min(abs(gp.nums - cut.seed))
+    cutoff <- floor( gp.nums[ i ] )
   }
-    
-  plot(x$mlog.evalue, xlab="Hit No", ylab="-log(Evalue)", col=gps, ...)
-  text(  gp.end, gp.mins, labels=gp.cut, col="gray50", pos=4, ...)
-  ## abline(h=gp.cut, col="gray70", lty=3)
-  abline(h=cutoff, col="red", lty=3)
-  plot(x$bitscore, xlab="Hit No", ylab="Bitscore", col=gps, ...)
-  plot(x$hit.tbl[,"identity"], xlab="Hit No", ylab="Identity", col=gps, ...)
-  plot(x$hit.tbl[,"alignmentlength"], xlab="Hit No", ylab="Length", col=gps, ...)
-  
+
+  cat("  * Chosen cutoff value of:   ", cutoff, "\n",
+      "           Yielding Nhits:   ", gp.inds[i], "\n")
+      
+
+  ##- Plot each alignment statistic with annotated grps
+  panelplot(gp=gp.inds)
+  panelplot(x$bitscore, ylab="Bitscore", gp=gp.inds)
+  panelplot(x$hit.tbl[,"identity"], ylab="Identity", gp=gp.inds)
+  panelplot(x$hit.tbl[,"alignmentlength"], ylab="Length", gp=gp.inds)
+
+
+  ##- Return details of hits above cutoff
   inds <- x$mlog.evalue >= cutoff
   out <- cbind("pdb.id"=x$pdb.id[inds], "gi.id"=x$gi.id[inds], "group"=gps[inds])
   rownames(out) <- which(inds)
   return(list(hits=out, pdb.id=x$pdb.id[inds], gi.id=x$gi.id[inds]))
-  ##return(out)
 }
