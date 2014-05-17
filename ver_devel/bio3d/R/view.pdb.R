4936877f8a05b97ad5bd3a7b92ff529695de4ba7
Barry Grant <bjgrant@umich.edu>
2014-05-17 01:31:36 -0400

    ENHANCEMENT: Adding new prototype view.pdb() and friends to package
    Please try out the examples in the man (Rd) file and try to break these please.

--- ver_devel/bio3d/R/view.pdb.R
+++ ver_devel/bio3d/R/view.pdb.R
@@ -1,112 +1,288 @@
-sse.color <- function(x, col.coil = "gray", col.helix = "purple", col.sheet = "blue") {
+sse.color <- function(x, atom.sel=NULL, col.coil="gray", col.helix="purple", col.sheet="yellow") {
   ##-- Define a color vector with helix and sheet 
   ##    annotations taken from input PDB file.
   ##      ToDo: - if no $helix and $sheet defined take 
-  ## 		        annotation from dssp() or stride()
+  ##            annotation from dssp() or stride()
+  ##
+  ## MV inside view.pdb() function
+  ##
   
-  col <- rep(col.coil, nrow(x$atom))
+#  if(!is.null(atom.sel)) {
+#    x <- trim.pdb(x, atom.sel)
+#  }
   
-  if(is.null(x$helix))
-    stop("'x$helix' is NULL. Please use 'dssp' or 'stride' to create it.")
-  else  
+  resno <- x$atom$resno
+  col <- rep(col.coil, length(resno))
+
+  if(is.null(x$helix) && is.null(x$sheet)) {
+    ## No SSE defined in PDB calling dssp()
+    ####x <- dssp(x)
+    if(is.null(x$helix) && is.null(x$sheet)) {
+      ## Still no SSE return coil color
+      return(col)
+    }
+  }
+
+  if(!is.null(x$helix$start)) {
     h.resno <- unbound(x$helix$start, x$helix$end)
-  
-  if(is.null(x$sheet))
-    stop("'x$sheet' is NULL. Please use 'dssp' or 'stride' to create it.")
-  else
+    col[(resno %in% h.resno)] = col.helix
+  }
+
+  if(!is.null(x$sheet$start)) {
     e.resno <- unbound(x$sheet$start, x$sheet$end)
+    col[(resno %in% e.resno)] = col.sheet
+  }
+
+  ## N.B. The above will not work with multi chain input
+  ##  i.e. PDB files with overlapping resno, in which 
+  ##   case we need to match both chain & resno 
+  ##   - See trim.pdb() for example code
   
-  h.ind <- which(as.numeric(x$atom$ersno) %in% h.resno)
-  e.ind <- which(as.numeric(x$atom$resno) %in% e.resno)
-  
-  col[h.ind] <- col.helix
-  col[e.ind] <- col.sheet
   return(col)
 }
 
-view <- function(...)
-  UseMethod("view")
+  vec2color <- function(vec, pal=c("blue", "green", "red"), n=30) {
+    ##-- Define a color scale from a numeric vector
+    ##     To Do - make independent of classInit package?
+    ##   (I think Julien posted a new version but I cant find it currently)
+    require(classInt)
+    return( findColours(classIntervals(vec, n=n, style="equal"), pal) )
+  }
 
-view.pdb <- function(pdb, sel = "default", col = NULL, cna = NULL, ...) {
-    ##-- Wrapper for visualize() to view larger PDBs the way Barry 
-    ##    likes to see them most often.
-    ##      To Do - This is a very quick and dirty prototype with 
-    ##              no consideration of efficiency or error checking. 
-    ##    For example, we use trim.pdb() rather than just using atom 
-    ##    selections and we don't check the 'col' input vector length 
-    ##    matches natom or convert it based on 'sel' input).
-    ##
-#     par.save <- par3d(skipRedraw=TRUE)
-#     on.exit(par3d(par.save))
-    
-    if(sel == "default") {
-        ca.sel <- atom.select(pdb, "calpha" , verbose = FALSE)
+
+view.pdb <- function(pdb, type="default", atom.sel=NULL, col=NULL, cna=NULL, ...) {
+   ##-- Wrapper for visualize() to view larger PDBs the way Barry 
+   ##    likes to see them most often.
+   ##      To Do - Check validity on "atom.sel" input 
+   ##            - Check validity of "col" input and add "keyword" color types
+   ##            - Add input more args, e.g. lwd=NULL, lwd.ca=3, lwd.nca=1 etc.
+   ##         - Add cna code.
+   ##
+   ##     N.B. In general this is still a quick and dirty prototype with 
+   ##          no consideration of efficiency and little error checking. 
+   ##
+
+
+   type.options <- c("default", "calpha", "back", "protein", "all")
+   type <- match.arg(type, type.options)
+
+   ##- Check on 'col' input 
+   ##   This section of code could be improved
+   ##   'col' could be a vector of colors or a "keyword" 
+   ##    e.g. "sse", "index", "atom", etc. ) 
+   if(is.null(col)) {
+    if(type=="all") {
+      ## Color by index
+      col <- vec2color(1:nrow(pdb$atom))
+    } else {
+      ## Color by secondary structure
+      col <- sse.color(pdb)
+    }
+   } else {
+    if(length(col) == 1) {
+      col=rep(col, nrow(pdb$atom))
+    }
+    if(length(col) != nrow(pdb$atom)) {
+      stop("Length of input color vector, 'col' does not match natom PDB")
+    }
+   }
+
+   if(!is.null(atom.sel)) {
+    pdb <- trim.pdb(pdb, atom.sel)
+    col <- col[atom.sel$atom]
+   }
+
+
+   if(type=="default") { 
+      ## Calpha trace plus sidechains and ligand
+      ca.sel   <- atom.select(pdb, "calpha", verbose = FALSE)
       prot.sel <- atom.select(pdb, "protein", verbose = FALSE)
-      back.sel <- atom.select(pdb, "back"   , verbose = FALSE)
-       lig.sel <- atom.select(pdb, "ligand" , verbose = FALSE)
-
-      connectivity(pdb) <- connectivity(pdb)
+      back.sel <- atom.select(pdb, "back", verbose = FALSE)
+      lig.sel  <- atom.select(pdb, "ligand", verbose = FALSE)
+      side.sel <- combine.sel(prot.sel, back.sel, op="NOT", verbose = FALSE)
+      side.sel <- combine.sel(side.sel, ca.sel, op="OR", verbose = FALSE)
+      
+      ## Bonds
+      if(!is.null(pdb$con)){
+        connectivity(pdb) <- connectivity(pdb)
+      }
 
       ## Ligand
       if(length(lig.sel$atom) != 0){
-        pdb.lig <- trim.pdb(pdb, lig.sel)
-        visualize(pdb.lig, con = FALSE, type="s") 
+          visualize(trim.pdb(pdb, lig.sel), con=FALSE, type="s")
       }
 
       ## Sidechain
-      tot.sel <- combine.sel(prot.sel, back.sel, op="NOT",verbose=FALSE)
-      tot.sel <- combine.sel( tot.sel,   ca.sel, op="OR" ,verbose=FALSE)
-      if(length(tot.sel$atom) != 0) {
-        pdb <- trim.pdb(pdb, tot.sel)
-        ca.con <- calpha.connectivity(pdb)
-#         connectivity(pdb) <- rbind(pdb$con, ca.con)
-        ## Problem with centre when using trim.pdb!!!
-        visualize(pdb, con = FALSE, add = TRUE, type = "l", col = "gray", lwd = 1)        
-      }
-     
-      # Calpha
-      ca.sel <- atom.select(pdb, "calpha" , verbose = FALSE)
-      if(length(ca.sel$atom) != 0) {
-        pdb <- trim.pdb(pdb, ca.sel)        
-        connectivity(pdb) <- calpha.connectivity(pdb)
-        if(is.null(col))
-          col <- sse.color(pdb, col.coil = "#808080")
-        visualize(pdb, con = FALSE, add = TRUE, type = "l", col = col)
-      }
+      if(length(side.sel$atom) != 0) {
+        visualize(trim.pdb(pdb, side.sel), con = FALSE, add = TRUE, type = "l", col = "gray", lwd = 1)
     }
-    if(sel=="calpha") { ## I like this view!
-      ## draw sse colored calpha trace
-      pdb.ca <- trim.pdb(pdb, atom.select(pdb,"calpha",verbose=FALSE))
-      connectivity(pdb.ca) <- calpha.connectivity(pdb.ca)
-      if(is.null(col))
-        col <- sse.color(pdb.ca)
-      if(length(pdb.ca$xyz)!=0)
-        visualize(pdb.ca, con=FALSE, col=col, ...)
-    } 
-    
-    if(sel=="protein") {
-      pdb.prot <- trim.pdb(pdb, atom.select(pdb,"protein",verbose=FALSE))
-      connectivity(pdb.prot) <- connectivity(pdb.prot)
-      if(length(pdb.prot$xyz)!=0)
-        visualize(pdb.prot, con = FALSE, ...)
-    }
-    if(sel=="back") {
-      back <- combine.sel( atom.select(pdb,"protein",verbose=FALSE), atom.select(pdb,"back",verbose=FALSE),verbose=FALSE )
-      pdb.prot <- trim.pdb(pdb, back)
-      connectivity(pdb.prot) <- connectivity(pdb.prot)
-      if(is.null(col))
-        col <- sse.color(pdb.prot)
-      if(length(pdb.prot$xyz)!=0)
-        visualize(pdb.prot, con = FALSE, col=col, ...)
-    }
-    
-    #if(!is.null(cna)) { 
-    #		visualize.cna(cna, pdb, ...) ## need to think more about this
-    #}
 
+    ## Calpha
+      if(length(ca.sel$atom) != 0) {
+        ca.pdb <- trim.pdb(pdb, ca.sel)
+        connectivity(ca.pdb) <- calpha.connectivity(ca.pdb)
+
+        col <- col[ca.sel$atom]
+
+      visualize(ca.pdb, con=FALSE, col=col, add = TRUE, type = "l", lwd=3, ...)
+    }
+    } 
+
+
+   if(type=="calpha") {
+    ## SSE colored C-alpha trace
+    ca.sel <- atom.select(pdb,"calpha", verbose=FALSE)
+    if(length(ca.sel$atom) != 0) {
+        ca.pdb <- trim.pdb(pdb, ca.sel)
+        connectivity(ca.pdb) <- calpha.connectivity(ca.pdb)
+
+        col <- col[ca.sel$atom]
+
+      visualize(ca.pdb, con=FALSE, col=col, type = "l", lwd=3, ...)
+    }
+   }
+   
+
+   if(type=="protein") {
+    ## Just protein
+      prot.sel <- atom.select(pdb, "protein", verbose = FALSE)
+      prot.pdb <- trim.pdb(pdb, prot.sel)
+      connectivity(prot.pdb) <- connectivity(prot.pdb)
+
+      col <- col[prot.sel$atom]
+
+      if(length(prot.pdb$xyz)!=0){
+        visualize(prot.pdb, con = FALSE, col=col, ...) ## col=col ?
+      }
+   }
+
+   
+   if(type=="back") {
+    back.sel <- atom.select(pdb,"back", verbose=FALSE)
+    back.pdb <- trim.pdb(pdb, back.sel)
+      connectivity(back.pdb) <- connectivity(pdb) ## <--- Using back.pdb here gives strange result!!!
+
+      col <- col[back.sel$atom] 
+ 
+    if(length(back.pdb$xyz)!=0) {
+      visualize(back.pdb, con=FALSE, col=col, ...)
+    }
+   }
+
+   if(type=="all") {
+    ## STILL A BUG HERE IN visualize() FOR LIGAND!! - Need to fix
+      ### visualize(pdb, con=FALSE, col=col,...) ###
+
+      ## Tmp fix draw ligand separately without lines
+      lig.sel  <- atom.select(pdb, "ligand", verbose = FALSE)
+      if(length(lig.sel$atom) != 0){
+          visualize(trim.pdb(pdb, lig.sel), con=FALSE, type="s")
+      }
+
+      prot.sel <- atom.select(pdb, "protein", verbose = FALSE)
+      prot.pdb <- trim.pdb(pdb, prot.sel)
+      connectivity(prot.pdb) <- connectivity(prot.pdb)
+
+      col <- col[prot.sel$atom]
+
+      if(length(prot.pdb$xyz)!=0){
+        visualize(prot.pdb, con = FALSE, add=TRUE, col=col, ...) ## col=col ?
+      }
+
+    }
+   #if(!is.null(cna)) { 
+   #    visualize.cna(cna, pdb, ...) ## need to think more about this
+   #}
 }
   
-view.character <- function(file, sel = "default", col = NULL, cna = NULL, ...) {
-  x <- read.pdb(file)
-  view.pdb(x, sel = sel, col = col, cna = cna, ...)
+
+view.pdbs <- function(x, type=1, col=NULL, add=FALSE, ...) {
+  ##-- Wrapper to visualize() for multiple structures
+
+  as.xyz <- function(x, nrow=1, ncol=length(x), byrow=TRUE) {
+    y <- matrix(as.numeric(x), nrow=nrow, ncol=ncol, byrow=byrow)
+    class(y)="xyz"
+    return(y)
+  }
+
+  if(class(x) == "3dalign") {
+    gap.ind <- is.gap(x$ali)
+    x <- x$xyz
+  } else {
+    ###gap.ind <- is.na( x[seq(1, to=length(x), by=3)] ) ##<-- Wrong !!
+    gap.ind <- NULL
+  }
+  nstru <- nrow(x)
+  npos  <- (ncol(x)/3)
+
+  ## -- The 'type' argument is for trying to sort out 'col' color specification 
+  ##     for different purposes.  Note. 'col' input could be: 
+  ##      1. a) single element vector to be applied to all structures, 
+  ##         b) a multiple element vector with a color per structure, 
+  ##      2. a) a vector with a color per atom, or
+  ##         b) a matrix with a column per atom position and row per structure
+  ## 
+  ##     This is specified by 'type=1' or 'type=2' 
+  ##     Eventually we want to be a bit smarter and remove the need for the 'type' argument 
+
+
+  ## Sort out color options with the aid of type argument
+  if(type==1) {
+    ## Option No. #1 above
+    if( is.null(col) ) {
+      col <- as.matrix( vmd.colors(nstru) )
+    } else {
+      if(length(col)==1) {
+        col <- as.matrix( rep(col, nstru) )
+      }
+      if(length(col) != nstru) {
+        stop("For type=1: Color vector should be the same length as the number of structures")
+      } else {
+        col <- as.matrix( col )
+      }
+    }
+  }
+  if(type==2) {
+    ## Option No. #2 above
+    if( is.null(col) ) {
+      col <- vec2color(1:npos)
+      col <- matrix(rep(col, nstru),ncol=npos,nrow=nstru, byrow=TRUE)
+    } else {
+      if( is.null(nrow(col)) ) {
+        ## We have an input 'col' vector we want to apply to all structures
+        if(length(col) == npos) {
+          cat("IN HERE\n\n")
+          col <- matrix(rep(col, nstru),ncol=npos,nrow=nstru, byrow=TRUE)
+          cat(dim(col))
+        }
+        else {
+          stop("For type=2: Color vector should be same length as ncol pdbs$ali")
+          ## unclear what the user might want here...
+        }
+      } else {
+        ## we have a color matrix
+        if(dim(col) != dim(x)) {
+          stop("For type=2: Color matrix should be same dim as pdbs$ali")
+          ## again unclear what the user might want here...
+        }
+      }
+    }
+    ## Mark gap/missing positions in color matrix for later exclusion
+    if( !is.null(gap.ind) )
+      col[gap.ind]=NA  ## <---- Trouble here if input is not a pdbs object!!
+    ##cat( paste(dim(col), collapse="  x  " ), "\n" )
+  }
+
+  for(i in 1:nstru) {
+    xt = as.xyz( na.omit(x[i,]) )
+    xcol = na.omit(col[i,])
+    ## cat( paste( "  ** length x:", (length(xt)/3), "  length col:", length(xcol),"\n") )
+    if(i==1) {
+      visualize(xt, con=calpha.connectivity(xt), add=add, col=xcol, ...)
+    } else {
+      visualize(xt, con=calpha.connectivity(xt), add=TRUE, col=xcol, ...)   
+    }
+  }
 }
+
+
