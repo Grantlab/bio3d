897279f76a54316ea90a6bb7782e2588b0fd3ed9
Xin-Qiu Yao <xinqiu.yao@gmail.com>
2014-08-22 17:55:09 -0400

    OTHER: minor update of new_funs/subopt.path()

--- new_funs/subopt.path.r
+++ new_funs/subopt.path.r
@@ -1,16 +1,27 @@
-######## This script calculates length distribution, node degeneracy, 
-######## and identifies potentially key residues based on 
-######## sub-optimal paths analysis of dynamical networks
+## This script intends to do post-processing of results from WISP, 
+## a software for suboptimal paths calculation in a correlation network.
+## It calculates comparative length distribution of paths and node degeneracy
+## for multiple networks.
+##
+## Arguments: 
+##   inp           path to WISP results
+##   state         names for multiple inputs, used to group and name outputs
+##   col           colors for outputs of different states
+##   pdb           reference PDB
+##   outprefix     prefix of output file names
+##   concise       if TRUE only show nodes on paths
+##   cutoff        cutoff of node degeneracy to show in outputs
+##   normalize     if TRUE node degeneracy is calculated as percentage of total number of paths 
 
-subopt.path <- function(path, state = 1:length(path), col=1:length(path), pdb = NULL, outprefix="", 
-   rm.gaps = TRUE, cutoff.npath = 50, normalize = TRUE) {
+subopt.path <- function(inp, state = 1:length(inp), col=1:length(inp), pdb = NULL, outprefix="", 
+   concise = TRUE, cutoff = 0.1, normalize = TRUE) {
    require(bio3d)
    require(ggplot2)
 
    out <- NULL
 
    # read sub-optimal path lengths
-   data <- lapply(path, function(x) readLines(file.path(x, "simply_formatted_paths.txt")))
+   data <- lapply(inp, function(x) readLines(file.path(x, "simply_formatted_paths.txt")))
    y <- lapply(data, function(x) as.numeric(unlist(lapply(strsplit(x, " "), "[", 1))) )
   
    factor <- rep(state, unlist(lapply(y, length)))
@@ -39,14 +50,15 @@ subopt.path <- function(path, state = 1:length(path), col=1:length(path), pdb = 
    }
    y0 <- y
  
-   if(rm.gaps) { 
+   if(concise) { 
       # re-number node to get more concise plot
       ii <- sort(unique(unlist(y)))
       y <- lapply(y, match, ii)
    } 
    
    factor <- rep(state, unlist(lapply(y, length)))
-   # plot node degeneracy
+
+   # plot node degeneracy (un-normalized)
    bw=0.9
    wd=bw/1.5
    df <- data.frame(State=as.factor(factor), Node=unlist(y))
@@ -57,9 +69,14 @@ subopt.path <- function(path, state = 1:length(path), col=1:length(path), pdb = 
 #   out$path <- y0
 #   names(out$path) <- state 
   
-   # identify potentially key residues 
+   # store node degeneracy 
    yp <- lapply(y0, table)
-   yy <- lapply(yp, function(x) x[x > cutoff.npath])
+    
+   if(normalize) {
+      yp <- lapply(yp, function(x) x/max(x))
+   }
+
+   yy <- lapply(yp, function(x) x[x >= cutoff])
    nyy <- unique(names(unlist(yy)))
    i <- as.numeric(nyy)
    nyy <- nyy[order(i)]
@@ -79,7 +96,7 @@ subopt.path <- function(path, state = 1:length(path), col=1:length(path), pdb = 
    names(o) <- state 
    out$degeneracy <- do.call(rbind, o)
    if(normalize) {
-      out$degeneracy <- out$degeneracy / max(out$degeneracy) 
+#      out$degeneracy <- out$degeneracy / max(out$degeneracy) 
       out$degeneracy <- round(out$degeneracy, digits=2)
    }
    write.csv(out$degeneracy, file = paste(outprefix, "degeneracy.csv", sep=""))
