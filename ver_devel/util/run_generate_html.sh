1f812abe4f37d62015194d76a17b1b9225835065
Xin-Qiu Yao <xinqiu.yao@gmail.com>
2014-09-27 19:44:41 -0400

    OTHER: minor update of util script for HTML generation

--- ver_devel/util/run_generate_html.sh
+++ ver_devel/util/run_generate_html.sh
@@ -16,29 +16,33 @@ if test $# -ge 1; then
 fi
 utildir=`pwd`
 
-# 0. Create a folder for working
+# 1. Create a folder for working
 workdir=sandbox/`date +%m%d%y.%H%M%S`
 mkdir -p $workdir; cd $workdir
 
-# 1. Get clean Bio3D folders
+# 2. Get clean Bio3D folders
 R CMD build $utildir/../bio3d
 tar xvfz bio3d*.tar.gz
 
-# 2. Build symbol links; it is necessary when calling devtools:::load_all
+# 3. Build symbol links; it is necessary when calling devtools:::load_all
 ln -s inst/CITATION ./bio3d/
 ln -s inst/examples ./bio3d/
 ln -s inst/matrices ./bio3d/
 
-# 3. remove dontrun tags to run all example codes
+# 4. remove dontrun tags to run all example codes
 sh $utildir/remove_dontrun.sh
 
-# 4. start an R session and run the commands to generate html files in ./html/
+# 5. start an R session and run the commands to generate html files in ./html/
 Rscript -e "options(device=x11)" \
         -e "library(staticdocs)" \
         -e "build_site(pkg='bio3d', site_path='html', examples=$example, launch=TRUE)"
 
-# 5. tidy up html files
+# 6. tidy up html files
 utildir=$utildir sh $utildir/tidy_html.sh
 
-# 6. refresh your browser
+# 7. create a link to the results
+rm $utildir/html
+ln -s $workdir/html $utildir
+
+# 8. refresh your browser
 
