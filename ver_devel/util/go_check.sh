8ef1acee6709758dd1671b9be7a5c2d8262e22db
Xin-Qiu Yao <xinqiu.yao@gmail.com>
2014-10-08 10:03:56 -0400

    OTHER: update util/go_check.sh for CRAN check

--- ver_devel/util/go_check.sh
+++ ver_devel/util/go_check.sh
@@ -33,16 +33,28 @@ done
 if test $check == "yes"; then
    # Use devtools to check as/not as CRAN
    if test $cran == "yes"; then
-      $Rscript -e "devtools::check('../bio3d', cran=TRUE)"
+      # replace devtools::r_env_vars to set NOT_CRAN = "false"
+      # it is important to make skip_on_cran() work
+      cat > __go_check_tmp.r <<EOF
+          r_env_vars <- function() {
+              c(R_LIBS = paste(.libPaths(), collapse = .Platform\$path.sep), 
+              CYGWIN = "nodosfilewarning", R_TESTS = "", NOT_CRAN = "false", 
+              TAR = devtools:::auto_tar())
+          }
+EOF
+      $Rscript -e 'source("__go_check_tmp.r")'\
+               -e 'assignInNamespace("r_env_vars", r_env_vars, "devtools")'\
+               -e 'devtools::check("../bio3d", cran=TRUE)'
+      rm -f __go_check_tmp.r
    else
-      $Rscript -e "devtools::check('../bio3d', cran=FALSE)"
+      $Rscript -e 'devtools::check("../bio3d", cran=FALSE)'
    fi
 fi
 
 # Build Windows version and check
 # NOTE: it will use winbuilder online and returns results by E-Mail.
 if test $win == "yes"; then
-   $Rscript -e "devtools::build_win('../bio3d')"
+   $Rscript -e 'devtools::build_win("../bio3d")'
 fi
 
 ## Old script to check with default R command s
