da9ff325b48f353ff9932bc5c7a33d7b84a79ae1
Lars Skjærven <larsss@gmail.com>
2014-10-19 15:21:09 +0200

    OTHER: changed tests to use local files - most tests can now be done with local files (i.e. transducin dataset or two HEL PDBs) - pdbsplit and and most of read.pdb still needs internet connection

--- ver_devel/bio3d/tests/testthat/test-rmsd.R
+++ ver_devel/bio3d/tests/testthat/test-rmsd.R
@@ -1,31 +1,43 @@
 context("Testing RMSD function")
 
-test_that("rmsd() gets the same results as VMD", {
-   skip_on_cran()
+test_that("rmsd() gets the same results as PyMOL", {
+  file <- system.file(c("examples/1hel.pdb", "examples/1dpx.pdb"), package="bio3d")
 
-   invisible(capture.output(pdbs <- pdbaln(c("1tag", "1as0"))))
-   inds <- gap.inspect(pdbs$xyz)$f.inds
+   invisible(capture.output(pdb.a <- read.pdb(file[1])))
+   invisible(capture.output(pdb.b <- read.pdb(file[2])))
 
-   rmsd <- rmsd(pdbs$xyz[1, ], pdbs$xyz[2, ], inds, inds, fit=TRUE, ncore=1)
-   rmsd0 <- 1.659 # VMD results
+   invisible(capture.output(inds.a <- atom.select(pdb.a, "calpha")))
+   invisible(capture.output(inds.b <- atom.select(pdb.b, "calpha")))
 
-   expect_equal(round(rmsd, 3), rmsd0)
+  rd1 <- rmsd(a=pdb.a$xyz, b=pdb.b$xyz, a.inds=inds.a$xyz, b.inds=inds.b$xyz, fit=FALSE)
+  rd2 <- rmsd(a=pdb.a$xyz, b=pdb.b$xyz, a.inds=inds.a$xyz, b.inds=inds.b$xyz, fit=TRUE)
+  
+  ## with pymol "pair_fit 1hel and name CA, 1dpx and name CA" = 0.293
+  rmsd1 <- 1.386
+  rmsd2 <- 0.293
+
+  expect_equal(round(rd1, 3), rmsd1)
+  expect_equal(round(rd2, 3), rmsd2)
 })
 
 test_that("rmsd() with ncore>1 works properly", {
-   skip_on_cran()
 
-   invisible(capture.output(pdbs <- pdbaln(c("1tag", "1as0", "1as2"))))
-   inds <- gap.inspect(pdbs$xyz)$f.inds
-
-   # check if ncore > 1 is really faster 
-   time1 <- system.time(rmsd1 <- rmsd(pdbs$xyz, a.inds=inds, ncore=1))
-   time2 <- system.time(rmsd2 <- rmsd(pdbs$xyz, a.inds=inds, ncore=NULL))
-   time1 <- time1["elapsed"]
-   time2 <- time2["elapsed"]
-
-   expect_equivalent(rmsd1, rmsd2)
-
+  file <- system.file(c("examples/1hel.pdb", "examples/1dpx.pdb"), package="bio3d")
+  invisible(capture.output(pdb.a <- read.pdb(file[1])))
+  invisible(capture.output(pdb.b <- read.pdb(file[2])))
+  
+  invisible(capture.output(inds.a <- atom.select(pdb.a, "calpha")))
+  invisible(capture.output(inds.b <- atom.select(pdb.b, "calpha")))
+     
+  ## check if ncore > 1 is really faster 
+  time1 <- system.time(rmsd1 <- rmsd(a=pdb.a$xyz, b=pdb.b$xyz, a.inds=inds.a$xyz, b.inds=inds.b$xyz, fit=TRUE, ncore=1))
+  time2 <- system.time(rmsd2 <- rmsd(a=pdb.a$xyz, b=pdb.b$xyz, a.inds=inds.a$xyz, b.inds=inds.b$xyz, fit=TRUE, ncore=2))
+  
+  ##time1 <- time1["elapsed"]
+  ##time2 <- time2["elapsed"]
+  
+  expect_equivalent(rmsd1, rmsd2)
+  
 #   cat("Speed up by", round((time1-time2)/time2, 1)*100, "%", sep="")
 #   if(getOption("cores") > 1)
 #      expect_true(time1 > time2)
