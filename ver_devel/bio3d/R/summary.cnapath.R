4eaebf26f1811502b848261282e1ea72979c2d36
Xin-Qiu Yao <xinqiu.yao@gmail.com>
2014-11-08 01:37:10 -0500

    NEW: functions for suboptimal path analysis and visulization
    - main function is cnapath()
    - summary.cnapath(), path length distribution and node degeneracy calculation; also take care of comparison between networks and 2D plot
    - view.cnapath(), 3D visulization of paths in VMD

---
+++ ver_devel/bio3d/R/summary.cnapath.R
@@ -0,0 +1,125 @@
+summary.cnapath <- function(pa, ..., pdb = NULL, label = NULL, col = NULL, 
+   plot = FALSE, concise = FALSE, cutoff = 0.1, normalize = TRUE) {
+   if(!requireNamespace("ggplot2", quietly = TRUE))
+      stop("Please install the ggplot2 package from CRAN")
+  
+   pa <- list(pa, ...)
+   if(!all(sapply(pa, inherits, "cnapath")))
+      stop("Input pa is not a 'cnapath' object")
+   
+   if(is.null(label)) label = 1:length(pa)
+   if(is.null(col)) col = 1:length(pa)
+
+   out <- list()
+   
+   # read node numbers on paths
+   y <- lapply(pa, function(x) unlist(x$path))
+   
+   # store node degeneracy 
+   node.deg <- lapply(y, table)
+   if(normalize) {
+      node.deg <- lapply(node.deg, function(x) x/max(x))
+   }
+
+   # find on-path node by the cutoff
+   yy <- lapply(node.deg, function(x) x[x >= cutoff])
+   onpath.node <- unique(names(unlist(yy)))
+   i <- as.numeric(onpath.node)
+   onpath.node <- onpath.node[order(i)]
+
+   # generate the node degeneracy table
+   o <- lapply(node.deg, function(x) {
+      x <- x[match(onpath.node, names(x))]
+      x[is.na(x)] <- 0
+      names(x) <- onpath.node 
+      x 
+   } )
+
+   # replace node id with pdb resid and resno
+   if(!is.null(pdb)) {
+      resno <- pdb$atom[pdb$calpha, "resno"]
+      resid <- aa321(pdb$atom[pdb$calpha, "resid"])
+      o <- lapply(o, function(x) {
+            n <- paste(resid[as.numeric(names(x))], resno[as.numeric(names(x))], sep="")
+            names(x) <- n
+            x 
+      } )
+   }
+
+   names(o) <- label
+   out$network <- label
+   out$num.paths <- sapply(pa, function(x) length(x$path))
+   out$hist <- lapply(pa, function(x) table(cut(x$dist, breaks=5, include.lowest = TRUE)))
+   out$degeneracy <- do.call(rbind, o)
+   if(normalize) out$degeneracy <- round(out$degeneracy, digits=2)
+   
+   if(plot) { 
+      gcol = col; names(gcol) = label
+      
+      ##- for path length distribution
+      y1 <- lapply(pa, "[[", "dist")
+      factor1 <- rep(label, sapply(y1, length))
+      bw1 = diff(range(unlist(y1))) * 0.02
+      wd1 = bw1 / 1.5
+      df1 <- data.frame(State=as.factor(factor1), Length=unlist(y1))
+      df1 <- cbind(df1, bw = bw1)
+
+      ##- for node degeneracy 
+      y2 <- lapply(pa, function(x) unlist(x$path))
+      if(!is.null(pdb)) y2 <- lapply(y2, function(x) resno[x])
+      if(concise) { 
+         # re-number node to get more concise plot
+         ii <- sort(unique(unlist(y2)))
+         y2 <- lapply(y2, match, ii)
+      }
+      factor2 <- rep(label, sapply(y2, length))
+      bw2 = 0.9  # must be less than 1
+      wd2 = bw2 / 1.5
+      df2 <- data.frame(State=as.factor(factor2), Node=unlist(y2))
+      df2 <- cbind(df2, bw = bw2)
+   
+      p1 <- ggplot2::ggplot(df1, ggplot2::aes(x=Length, group=State, color=NULL, fill=State, bw=bw)) + 
+        ggplot2::geom_histogram(ggplot2::aes(y=(..density..)*bw), alpha=0.8, 
+        position = ggplot2::position_dodge(width=wd1), binwidth=bw1) + 
+        ggplot2::scale_fill_manual(values=gcol) + 
+        ggplot2::ylab("Probability") + 
+        ggplot2::ggtitle("Path length distribution") + ggplot2::theme_bw()
+
+      p2 <- ggplot2::ggplot(df2, ggplot2::aes(x=Node, group=State, color=NULL, fill=State, bw=bw)) + 
+         ggplot2::geom_histogram(ggplot2::aes(y=..count..), alpha=0.8, 
+            position=ggplot2::position_dodge(width=wd2), binwidth=bw2) + 
+               ggplot2::scale_fill_manual(values=gcol)+
+               ggplot2::ylab("Degeneracy") + 
+               ggplot2::ggtitle("Node degeneracy") + ggplot2::theme_bw()
+  
+      pushViewport(viewport(layout = grid.layout(1, 2)) )
+      print(p1, vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
+      print(p2, vp = viewport(layout.pos.row = 1, layout.pos.col = 2))
+   } 
+
+   return(out)
+}
+
+print.cnapath <- function(x, ...) {
+   dots = list(...)
+   normalize = TRUE
+   if("normalize" %in% names(dots))
+      normalize = dots$normalize
+   o <- summary(x, ...)
+    
+   cat("Number of networks: ", length(o$network), "(", 
+       paste(o$network, collapse=", "), ")\n")
+   cat("Number of paths in each network:",
+       paste("   ", o$network, ": ", o$num.paths, sep="", collapse="\n"), sep="\n")
+   cat("Path length distribution: \n")
+   for(i in 1:length(o$network)) {
+       cat("   --- ", o$network[i], " ---")
+       print(o$hist[[i]])
+       cat("\n")
+   }
+   cat("Node degeneracy table: \n\n")
+   if(normalize)
+      print(format(o$degeneracy, nsmall=2), quote=FALSE)
+   else 
+      print(o$degeneracy)
+}
---
+++ ver_devel/bio3d/R/view.cnapath.R
@@ -0,0 +1,112 @@
+view.cnapath <- function(pa, pdb, out.prefix = "cnapath_", launch = FALSE) {
+
+   if(!inherits(pa, "cnapath")) 
+      stop("Input pa is not a 'cnapath' object")
+
+   file = paste(out.prefix, "view.vmd", sep="")
+   pdbfile = paste(out.prefix, "view.pdb", sep="")
+
+   res <- unique(unlist(pa$path))
+   ind.source <- match(pa$path[[1]][1], res)
+   ind.sink <- match(pa$path[[1]][length(pa$path[[1]])], res)
+   
+   rmin <- min(pa$dist)
+   rmax <- max(pa$dist)
+   rad <- function(r, rmin, rmax, radmin = 0.01, radmax = 0.5) {
+      (rmax - r) / (rmax - rmin) * (radmax - radmin) + radmin
+   }
+
+   cols <- colorRampPalette(c("blue", "red"))(256)
+   col.mat <- matrix(NA, length(res), length(res))
+   conn <- matrix(0, length(res), length(res))
+   rr <- conn
+   for(j in 1:length(pa$path)) {
+      x = pa$path[[j]]
+      for(i in 1:(length(x)-1)) {
+         i1 = match(x[i], res)
+         i2 = match(x[i+1], res)
+         if(conn[i1, i2] == 0) conn[i1, i2] = conn[i2, i1] = 1
+         r = rad(pa$dist[j], rmin, rmax)
+         ic = (rmax - pa$dist[j]) / (rmax - rmin) * 255 + 1
+         col = cols[ic]
+         if(r > rr[i1, i2]) {
+            rr[i1, i2] = rr[i2, i1] = r
+            col.mat[i1, i2] = col.mat[i2, i1] = col
+         }
+      }
+   }
+ 
+   ca.inds <- atom.select(pdb, "calpha", verbose = FALSE)
+   res.pdb <- pdb$atom[ca.inds$atom[res], "resno"] 
+   
+   rownames(conn) <- res
+   colnames(conn) <- res
+   rownames(rr) <- res
+   colnames(rr) <- res
+
+   k = 500
+
+   cat("", file=file)
+   for(i in 1:(nrow(conn)-1)) {
+      for(j in (i+1):ncol(conn)) {
+         if(conn[i, j] == 1) {
+            col = as.numeric(col2rgb(col.mat[i, j]))/255
+            cat("color change rgb ", k, " ", paste(col, collapse=" "), "\n", sep="", file=file, append=TRUE)
+            cat("graphics top color ", k, "\n", sep="", file=file, append=TRUE)
+            cat("draw cylinder {", pdb$xyz[atom2xyz(ca.inds$atom[res[i]])], 
+               "} {", pdb$xyz[atom2xyz(ca.inds$atom[res[j]])], "} radius", rr[i, j], 
+               " resolution 6 filled 0\n", sep=" ", file=file, append=TRUE)
+            k = k + 1
+         }
+      }
+   } 
+#   cat("graphics top color 1\n", file=file, append=TRUE)
+#   opt.path = raw[which.min(raw[1,]), ]
+#   if(is.matrix(opt.path)) {
+#      warning("Multiple optimal paths. Take the first one")
+#      opt.path = opt.path[1,]
+#   }
+#   opt.path = opt.path[!is.na(opt.path)]
+#   for(i in 2:(length(opt.path)-1)) {
+#      j = i + 1
+#      cat("draw cylinder {", pdb$xyz[atom2xyz(opt.path[i]+1)], 
+#         "} {", pdb$xyz[atom2xyz(opt.path[j]+1)], "} radius 0.5", 
+#         " resolution 6 filled 0\n", sep=" ", file=file, append=TRUE)
+#   }
+#mol selection {(resid ", paste(opt.path[2:length(opt.path)]+1, collapse=' '), 
+   cat("mol representation VDW 6 0.1
+mol color colorID 0
+mol selection {(resid ", paste(res.pdb, collapse=' '), ") and name CA}
+mol addrep top
+mol new ", pdbfile, " type pdb first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
+mol delrep 0 top
+mol representation NewCartoon 0.300000 10.000000 4.100000 0
+mol color colorID 8
+mol selection {all}
+mol material Opaque
+mol addrep top
+mol representation Licorice 0.300000 10.000000 10.000000
+mol color name
+mol selection {(resid ", res.pdb[ind.source], " ", res.pdb[ind.sink], ")}
+mol material Opaque
+mol addrep top 
+", file=file, append=TRUE)
+
+   write.pdb(pdb, file=pdbfile)
+   
+   if(launch) {
+      
+      cmd <- paste("vmd -e", file)
+
+      os1 <- .Platform$OS.type
+      if (os1 == "windows") {
+        shell(shQuote(cmd))
+      } else{
+        if(Sys.info()["sysname"]=="Darwin") {
+          system(paste("/Applications/VMD\\ 1.9.*app/Contents/MacOS/startup.command -e", vmdfile))
+        } else {
+          system(cmd)
+        }
+      }
+   }
+}
