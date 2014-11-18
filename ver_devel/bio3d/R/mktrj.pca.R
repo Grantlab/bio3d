3a784fb1e1825bbffb4309fc605edfbd30c7f3a7
Lars Skjærven <larsss@gmail.com>
2014-11-18 16:59:56 +0100

    OTHER: update of Rd files for atom.select() and mktrj()

--- ver_devel/bio3d/R/mktrj.pca.R
+++ ver_devel/bio3d/R/mktrj.pca.R
@@ -1,4 +1,4 @@
-"mktrj.pca" <- function(x=NULL,   # pca data structure
+"mktrj.pca" <- function(pca=NULL,   # pca data structure
                         pc=1,       # which pc to move along
                         mag=1,      # magnification factor
                         step=0.125, # step size
@@ -6,7 +6,7 @@
                         ... ) {     # args for write.pdb
 
   ## make a trjactory of atomic displacments along a given pc
-  if(class(x)!="pca") {
+  if(class(pca)!="pca") {
     stop("input should be a list object of class 'pca' (from 'pca.xyz')")
   }
 
@@ -14,19 +14,19 @@
     file <- paste("pc_", pc, ".pdb", sep="")
   
   nstep <- c(seq(step, to=mag, by=step))
-  zcoor  <- cbind(sqrt(x$L[pc])) %*% nstep
+  zcoor  <- cbind(sqrt(pca$L[pc])) %*% nstep
 
   ##- Bug fix: Fri Jun 15 14:49:24 EDT 2012
   ##  plus  <- apply(zcoor, 2, pca.z2xyz, pca)
   ##  minus <- apply( (-(zcoor)), 2, pca.z2xyz, pca)
 
   scor  <- function(x,u,m) { return(x*u+m) }
-  plus  <- sapply(c(zcoor), scor, u=x$U[,pc], m=x$mean)
-  minus <- sapply(c(-zcoor), scor, u=x$U[,pc], m=x$mean)
+  plus  <- sapply(c(zcoor), scor, u=pca$U[,pc], m=pca$mean)
+  minus <- sapply(c(-zcoor), scor, u=pca$U[,pc], m=pca$mean)
 
-  coor  <- t(cbind(x$mean,
+  coor  <- t(cbind(pca$mean,
                    plus, plus[,rev(1:ncol(plus))],
-                   x$mean,
+                   pca$mean,
                    minus, minus[,rev(1:ncol(minus))]))
 
   write.pdb(xyz=coor, file=file, ...)
