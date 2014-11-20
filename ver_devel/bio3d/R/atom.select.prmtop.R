8681c59766f60b6b23862cb26e91c21898b5cbab
Lars Skjærven <larsss@gmail.com>
2014-11-20 19:56:24 +0100

    BUGFIX: bugs related to amb2pdb --> as.pdb rename

--- ver_devel/bio3d/R/atom.select.prmtop.R
+++ ver_devel/bio3d/R/atom.select.prmtop.R
@@ -1,9 +1,9 @@
 "atom.select.prmtop" <- function(prmtop, ...) {
   if(!inherits(prmtop, "prmtop"))
     stop("provide a PRMTOP object as obtained from read.prmtop()")
-  
+
   cl <- match.call()
-  tmp.pdb <- amb2pdb(prmtop, crd=rep(NA, prmtop$POINTERS[1]*3))
+  tmp.pdb <- as.pdb.prmtop(prmtop, crd=rep(NA, prmtop$POINTERS[1]*3))
   sele <- atom.select.pdb(tmp.pdb, ...)
   sele$call <- cl
   return(sele)
