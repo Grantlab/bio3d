f0834a3099340cc3e57fed2ab8da460dc74d6409
Xin-Qiu Yao <xinqiu.yao@gmail.com>
2014-12-08 14:07:45 -0500

    OTHER: update of plot.diff.cmap for separated x/y axis SSE annotation

--- new_funs/plot.diff.cmap.R
+++ new_funs/plot.diff.cmap.R
@@ -1,4 +1,4 @@
-plot.diff.cmap <- function(x, y, col=c("red", "blue"), col.common = "gray80", ...) {
+plot.diff.cmap <- function(x, y, col=c("red", "blue"), sse=NULL, sse.y=sse, col.common = "gray80", ...) {
 
   if(any(dim(x) != dim(y))) 
      stop("Contact maps to compare have different dimension")
@@ -16,14 +16,35 @@ plot.diff.cmap <- function(x, y, col=c("red", "blue"), col.common = "gray80", ..
 
   op = par(no.readonly = TRUE)
   dots = list(...)
+  # reset x/y labels for this function
+  if(is.null(dots$xlab)) dots$xlab = "Residue index for protein 1"
+  if(is.null(dots$ylab)) dots$ylab = "Residue index for protein 2"
 
-  # plot the common cmap 
+  # plot the common cmap; On this step, we add axes, sse (if available) and all labels.
+  dots2 = dots
   if(!is.null(dots$cex)) cex = dots$cex / 2
   else cex = 0.5
-  dots2 = dots
   dots2$cex = cex 
   dots2$pch = 20
-  do.call(plot.cmap, c(list(x=comm, col=col.common), dots2))
+  # bottom sse and data, but no annotatios and axes
+  dots2$left = FALSE
+  dots2$axes = FALSE
+  dots2$ann = FALSE
+  do.call(plot.cmap, c(list(x=comm, col=col.common, sse=sse), dots2))
+  # left sse and annotations/axes, but no data
+  dots2$left = dots$left
+  dots2$bot = FALSE
+  dots2$axes = dots$axes
+  dots2$ann = dots$ann
+  dots2$type = "n"
+  par(new=TRUE) 
+  do.call(plot.cmap, c(list(x=comm, col=col.common, sse=sse.y), dots2))
+  par(op)
+  
+  # don't repeat title, labels and x/y axes
+  dots$ann=FALSE
+  dots$axes=FALSE
+  dots$sse=NULL
 
   # plot the first cmap
   par(new=TRUE) 
