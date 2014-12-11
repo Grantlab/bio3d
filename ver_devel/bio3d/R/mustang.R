fb8204884c3194649803d73ff4b1b3f464f93ebe
Lars Skjærven <larsss@gmail.com>
2014-12-11 19:41:01 +0100

    BUGFIX: mustang outfile argument

--- ver_devel/bio3d/R/mustang.R
+++ ver_devel/bio3d/R/mustang.R
@@ -18,7 +18,7 @@
       dir.create(cleandir)
     newfiles <- c()
     for(i in 1:length(files)) {
-      outfile <- paste(cleandir, basename(files[i]), sep="/")
+      tmpout <- paste(cleandir, basename(files[i]), sep="/")
       pdb     <- read.pdb(files[i])
       sele    <- atom.select(pdb, "calpha", verbose=verbose)
       new     <- trim.pdb(pdb, sele)
@@ -26,14 +26,14 @@
       seq3     <- aa123(seq1)
       new$atom$type  <- "ATOM"
       new$atom$resid <- seq3
-      write.pdb(new, file=outfile)
-      newfiles <- c(newfiles, outfile)
+      write.pdb(new, file=tmpout)
+      newfiles <- c(newfiles, tmpout)
     }
     files <- newfiles
   }
   
   infile <- tempfile()
-  outfile <- tempfile()
+  tmpout <- tempfile()
   dirn <- unique(dirname(files))
 
   if(length(dirn)>1)
@@ -50,7 +50,7 @@
   write.table(rawlines, file=infile, quote=FALSE,
               row.names=FALSE, col.names=FALSE)
   
-  cmd <- paste(exefile, "-f", infile, "-o", outfile, "-F fasta")
+  cmd <- paste(exefile, "-f", infile, "-o", tmpout, "-F fasta")
 
   if(verbose)
     cat("Running command\n", cmd, "\n")
@@ -64,11 +64,11 @@
     stop(paste("An error occurred while running command\n '",
                exefile, "'", sep=""))
   
-  aln <- read.fasta(paste(outfile, ".afasta", sep=""))
+  aln <- read.fasta(paste(tmpout, ".afasta", sep=""))
   rownames(aln$ali) <- paste(dirn, rownames(aln$ali), sep="/")
   aln$id <- rownames(aln$ali)
 
-  unlink(infile); unlink(outfile);
+  unlink(infile); unlink(tmpout);
 
   if(!is.null(outfile))
     write.fasta(aln, file=outfile)
