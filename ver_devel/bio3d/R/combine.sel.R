26a2eb309796111ce828628226946fe88a0ab574
Xin-Qiu Yao <xinqiu.yao@gmail.com>
2014-12-15 17:29:34 -0500

    ENHANCEMENT: combine.sel() supports multiple (>2) atom selects as input

--- ver_devel/bio3d/R/combine.sel.R
+++ ver_devel/bio3d/R/combine.sel.R
@@ -1,33 +1,43 @@
 combine.sel <- 
- function(sel1=NULL, sel2=NULL, op="AND", verbose=TRUE) {
-   if(is.null(c(sel1, sel2))) {
-      stop("Invalid selection")
-   }
-   if(is.null(sel1))
-      return(sel2)
-   if(is.null(sel2))
-      return(sel1)
+ function(sel1=NULL, sel2=NULL, ..., op="AND", verbose=TRUE) {
+   sels <- list(sel1, sel2, ...)
+
+   if(any(sapply(sels, function(x) !is.null(x) && !inherits(x, "select"))))
+      stop("Invalid atom select(s)")
+
+   rm.inds = sapply(sels, is.null)
+   sels = sels[!rm.inds]
+
+   if(length(sels) == 0)
+      return(NULL)
+   else if(length(sels) == 1)
+      return(sels[[1]])
  
    op.tbl <- c(rep("AND",3), rep("OR",4), rep("NOT",4))
    op <- op.tbl[match(op, c("AND","and","&","OR","or","|","+","NOT","not","!","-"))]
-   sel <- switch(op, 
-      "AND"= {
-         if(verbose) { cat(" Intersection of sel1 and sel2\n") }
-         list(atom = intersect(sel1$atom, sel2$atom), 
-              xyz = intersect(sel1$xyz, sel2$xyz))
-      },
-      "OR" = {
-        if(verbose) { cat("Union of sel1 and sel2\n") }
-         list(atom = sort(union(sel1$atom, sel2$atom)), 
-              xyz = sort(union(sel1$xyz, sel2$xyz)))
-      },
-      "NOT" = {
-        if(verbose) { cat("Sel2 is subtracted from sel1\n") }
-         list(atom = setdiff(sel1$atom, sel2$atom), 
-               xyz = setdiff(sel1$xyz, sel2$xyz))
-      },
-      stop("Unknown operation") )
+   
+   # Print message
+   if(verbose) {
+      msg <- switch(op,
+        AND = " Intersect of selects",
+        OR  = " Union of selects",
+        NOT = " Select 2 (, 3, ...) is subtracted from select 1",
+        stop("Unknown operation") )
+      cat(msg, "\n", sep="")
+   }
+ 
+   sel <- sels[[1]]$atom
+   for(i in 2:length(sels)) {
+      sel <- 
+         switch(op, 
+         "AND" = intersect(sel, sels[[i]]$atom), 
+         "OR"  = sort(union(sel, sels[[i]]$atom)),
+         "NOT" = setdiff(sel, sels[[i]]$atom),
+         stop("Unknown operation") )
+   }
+   sel <- list(atom = sel, xyz = atom2xyz(sel))
    if(verbose) { cat(paste(" *  Selected a total of:", length(sel$atom), "atoms  *\n")) }
-   class(sel)="select"
+
+   class(sel) = "select"
    return(sel)
 }
