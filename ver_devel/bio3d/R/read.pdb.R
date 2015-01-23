f7123e08b247cbdff763b31c7b927a776b7e3af1
Xin-Qiu Yao <xinqiu.yao@gmail.com>
2015-01-22 19:55:18 -0500

    BUGFIX: minor change of read.pdb() for empty SSE records

--- ver_devel/bio3d/R/read.pdb.R
+++ ver_devel/bio3d/R/read.pdb.R
@@ -137,28 +137,34 @@ function (file, maxlines=-1, multi=FALSE,
   ## End Edit from Baoqiang:
 
   ##- Secondary structure
-  helix  <- list(start = as.numeric(substring(raw.helix,22,25)),
-                 end   = as.numeric(substring(raw.helix,34,37)),
-                 chain = trim(substring(raw.helix,20,20)),
-                 type  = trim(substring(raw.helix,39,40)))
-
-  ##- insert code for initial and end residues of helices
-  insert.i <- trim(substring(raw.helix,26,26))
-  insert.e <- trim(substring(raw.helix,38,38))
-  names(helix$start) <- insert.i
-  names(helix$end) <- insert.e
-
-  sheet  <- list(start = as.numeric(substring(raw.sheet,23,26)),
-                 end   = as.numeric(substring(raw.sheet,34,37)),
-                 chain = trim(substring(raw.sheet,22,22)),
-                 sense = trim(substring(raw.sheet,39,40)))
-
-  ##- insert code for initial and end residues of sheets
-  insert.i <- trim(substring(raw.sheet,27,27))
-  insert.e <- trim(substring(raw.sheet,38,38))
-  names(sheet$start) <- insert.i
-  names(sheet$end) <- insert.e
-
+  if(length(raw.helix) > 0) {
+     helix  <- list(start = as.numeric(substring(raw.helix,22,25)),
+                    end   = as.numeric(substring(raw.helix,34,37)),
+                    chain = trim(substring(raw.helix,20,20)),
+                    type  = trim(substring(raw.helix,39,40)))
+   
+     ##- insert code for initial and end residues of helices
+     insert.i <- trim(substring(raw.helix,26,26))
+     insert.e <- trim(substring(raw.helix,38,38))
+     names(helix$start) <- insert.i
+     names(helix$end) <- insert.e
+  } else {
+     helix <- NULL
+  }
+  if(length(raw.sheet) > 0) {
+     sheet  <- list(start = as.numeric(substring(raw.sheet,23,26)),
+                    end   = as.numeric(substring(raw.sheet,34,37)),
+                    chain = trim(substring(raw.sheet,22,22)),
+                    sense = trim(substring(raw.sheet,39,40)))
+   
+     ##- insert code for initial and end residues of sheets
+     insert.i <- trim(substring(raw.sheet,27,27))
+     insert.e <- trim(substring(raw.sheet,38,38))
+     names(sheet$start) <- insert.i
+     names(sheet$end) <- insert.e
+  } else {
+     sheet <- NULL
+  }
 
  ## 2014-04-23
  ## Update to use single data.frame for ATOM and HETATM records
