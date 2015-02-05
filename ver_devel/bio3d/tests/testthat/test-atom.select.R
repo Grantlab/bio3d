eb0436fb681690442e7eb5125dd490e92ded5993
Xin-Qiu Yao <xinqiu.yao@gmail.com>
2015-02-05 10:38:51 -0500

    OTHER: update of test function for atom.select()
    - Now, read.pdb() removes 'alt' automatically
    - Do we still need a test for a 'pdb' contains 'alt' coords?

--- ver_devel/bio3d/tests/testthat/test-atom.select.R
+++ ver_devel/bio3d/tests/testthat/test-atom.select.R
@@ -4,7 +4,8 @@ test_that("atom.select() gets correct selections with various options", {
 
   # Use the curated test-purpose pdb file
   file <- system.file("examples/test.pdb", package="bio3d")
-  invisible(capture.output(pdb <- read.pdb(file, rm.alt = FALSE)))
+#  invisible(capture.output(pdb <- read.pdb(file, rm.alt = FALSE)))
+  invisible(capture.output(pdb <- read.pdb(file)))
 
   # Select all atoms: omit 1 ALT
   capture.output(all.inds <- atom.select(pdb, "all"))
@@ -14,8 +15,8 @@ test_that("atom.select() gets correct selections with various options", {
   capture.output(a.inds <- atom.select(pdb, chain = "A"))
   expect_equal(a.inds$xyz, atom2xyz(a.inds$atom))  # self-consistent
   expect_equal(length(a.inds$atom), 98)
-  expect_equal(a.inds$atom[1], 1) 
-  expect_equal(a.inds$atom[98], 171) 
+  expect_equal(a.inds$atom[1], 1)
+  expect_equal(a.inds$atom[98], 170)
 
   # Select protein: omit 1 ALT
   #                 omit 2 'unknown' AA
@@ -51,7 +52,7 @@ test_that("atom.select() gets correct selections with various options", {
   expect_equal(length(wat.inds), 5)
 
   capture.output(ion.inds <- atom.select(pdb, resid = "CA")$atom)
-  expect_equal(ion.inds, 171)
+  expect_equal(ion.inds, 170)
 
   capture.output(gdp.inds <- atom.select(pdb, resid = "GDP")$atom)
   expect_equal(length(gdp.inds), 40)
@@ -69,26 +70,26 @@ test_that("atom.select() gets correct selections with various options", {
   capture.output(npro2.inds <- atom.select(pdb, "protein", inverse = TRUE)$atom)
   expect_equal(npro.inds, npro2.inds)
   expect_equal(length(intersect(pro.inds, npro.inds)), 0)
-  expect_equal(length(pro.inds) + length(npro.inds), nrow(pdb$atom) - 1) # omit ALT
+  expect_equal(length(pro.inds) + length(npro.inds), nrow(pdb$atom)) # omit ALT
   
   capture.output(nwat.inds <- atom.select(pdb, "notwater")$atom)
   capture.output(nwat2.inds <- atom.select(pdb, "water", inverse = TRUE)$atom)
   expect_equal(nwat.inds, nwat2.inds)
   expect_equal(length(intersect(wat.inds, nwat.inds)), 0)
-  expect_equal(length(wat.inds) + length(nwat.inds), nrow(pdb$atom) - 1) # omit ALT
+  expect_equal(length(wat.inds) + length(nwat.inds), nrow(pdb$atom)) # omit ALT
    
   capture.output(noh.inds <- atom.select(pdb, "noh")$atom)
   capture.output(noh2.inds <- atom.select(pdb, "h", inverse = TRUE)$atom)
   expect_equal(noh.inds, noh2.inds)
   expect_equal(length(intersect(h.inds, noh.inds)), 0)
-  expect_equal(length(h.inds) + length(noh.inds), nrow(pdb$atom) - 1) # omit ALT
+  expect_equal(length(h.inds) + length(noh.inds), nrow(pdb$atom)) # omit ALT
   
   # Test on combination of select
   capture.output(comb1.inds <- atom.select(pdb, chain = "B", resno = c(1,4)))
   expect_equal(length(comb1.inds$atom), 33) # omit ALT
   
   capture.output(comb2.inds <- atom.select(pdb, resid = "GDP", elety = "PA") )
-  expect_equal(comb2.inds$atom, 136)
+  expect_equal(comb2.inds$atom, 135)
 
   capture.output(comb3.inds <- atom.select(pdb, "noh", resid = "TES", chain = "A") )
   expect_equal(comb3.inds$atom, c(8, 10, 12, 16, 17)) 
