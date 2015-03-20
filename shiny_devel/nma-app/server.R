4a749081a604d76a529ceaa427e02b4461c6d480
Lars Skjærven <larsss@gmail.com>
2015-03-21 00:04:06 +0100

    OTHER: update of shiny NMA

--- shiny_devel/nma-app/server.R
+++ shiny_devel/nma-app/server.R
@@ -1,156 +1,264 @@
+#options(rgl.useNULL=TRUE) ## Required for a headless server
 library(shiny)
-library(bio3d)
+library(shinyRGL)
+library(rgl)
 
-# Define server logic required to calculate normal modes
-# and plot fluctuations
+## Define server logic for NMA shiny demo
+## TODO: Simplify!
+
 shinyServer(function(input, output, session) {
 
 
-    fetchPDB <- reactive({
-        if(nchar(input$pdbid)==4) {
-            print(input)
-            file <- get.pdb(input$pdbid)
-            pdb <- read.pdb(file)
-        }
-        else {
-            stop("Provide a 4 character PDB code")
-        }
-        return(pdb)
-    })
+  ##- PDB input UI that is responsive to 'reset button' below
+  output$resetable_pdb_input <- renderUI({
+    ## 'input$reset_pdb_input' is just used as a trigger for reset
+    reset <- input$reset_pdb_input
+    textInput("pdbid", label="Enter RCSB PDB code/ID:", value = "4Q21") #)
+  })
 
 
-    checkPDB <- reactive({
-        pdb <- fetchPDB()
 
-        if(input$chain!="") {
-            chain <- input$chain
-            chain <- gsub(" ", "", chain)
-            chain <- gsub(",", "", chain)
-            chain <- unlist(strsplit(chain, ""))
+  finalPDB <- function() {
+    pdb <<- fetchPDB()
 
-            if(chain[1] != "") {
-                sele <- atom.select(pdb, chain=chain)
+    ## unmodified PDB, CONFUSING
+    pdb2 <<- pdb
+    chains <<- chainPDB()
 
-                if(!length(sele$atom)>0)
-                    stop("insufficient atoms in selection")
+    if(is.vector(input$chains))
+      pdb <<- trimPDB()
 
-                pdb <- trim.pdb(pdb, sele)
-            }
-        }
+    return(pdb)
+  }
+    
 
-        max_atoms <- 500
-        min_atoms <- 2
+  fetchPDB <- reactive({
+    if(nchar(input$pdbid)==4) {
+      file <- get.pdb(input$pdbid)
+      pdb <- read.pdb(file, verbose=FALSE)
+      return(pdb)
+    }
+    else {
+      stop("Provide a 4 character PDB code")
+    }
+  })
+    
+  trimPDB <- reactive({
+    ##message("Trimming PDB")
+    if(is.vector(input$chains)) {
+      pdb <- trim(pdb, chain=input$chains)
+      return(pdb)
+    }
+  })
+  
+  chainPDB <- reactive({
+    invisible(capture.output( pdb2 <- fetchPDB() ))
+    chains <- unique(trim(pdb2, "protein", elety="CA")$atom$chain)
+    names(chains) <- chains
+    return(chains)
+  })
+  
+  output$pdbSummary <- renderPrint({
+    input$pdbaction
+     
+    invisible(capture.output( pdb <- finalPDB() ))
+    print(pdb)
+  })
 
-        if(sum(pdb$calpha) > max_atoms)
-            stop(paste(">", max_atoms, "residues not allowed"))
-        if(sum(pdb$calpha) < min_atoms)
-            stop(paste("<", min_atoms, "residues not allowed"))
+  ## all available chains in PDB
+  output$chains1 <- renderPrint({
+  input$pdbaction
+    
+    invisible(capture.output(  chains <- chainPDB() ))
+     cat( chains, sep=", ")
+  })
 
-        return(pdb)
-    })
+
+  ## checkbox 
+  output$chains2 <- renderUI({
+    input$pdbaction
+    
+    chains <- chainPDB()
+    checkboxGroupInput("chains", label = "Limit to chain IDs:", 
+                       choices = chains)
+                       ##selected = chains[1:length(chains)])
+  })
+  
 
 
-    calcModes <- reactive({
-        pdb <- checkPDB()
+  ########################
+  ##-- NMA Calculation   #
+  ########################s
+  output$resetable_nma_input <- renderUI({
+    ## used as a trigger for reset
+    reset <- input$reset_nma_input
+    div(
+    selectInput("forcefield", "Choose a forcefield:",
+      choices = c("calpha", "anm", "pfanm", "sdenm")),
 
-        temp <- as.numeric(input$temp)
-        if(temp==0)
-            temp <- NULL
 
-        if(input$mass=="Yes")
-            mass <- TRUE
-        else
-            mass <- FALSE
+    sliderInput("cutoff", "Cutoff value:",
+      min = 6, max = 50, value = 10),
 
-        if(input$forcefield %in% c("calpha", "sdenm"))
-            modes <- nma(pdb, ff=input$forcefield, mass=mass, temp=temp)
+    sliderInput("temp", "Temperature scaling:",
+      min = 0, max = 350, value = 300),
 
-        if(input$forcefield %in% c("anm", "pfanm"))
-            modes <- nma(pdb, ff=input$forcefield, cutoff=input$cutoff,
-                         mass=mass, temp=temp)
+    checkboxInput("mass", "Mass-weighting", value=TRUE) )
+  })
 
-        return(modes)
+  calcModes <- reactive({
+    pdb <- finalPDB()
+    
+    temp <- as.numeric(input$temp)
+    if(temp==0)
+      temp <- NULL
+    
+    if(input$forcefield %in% c("calpha", "sdenm"))
+      modes <<- nma(pdb, ff=input$forcefield, mass=input$mass, temp=temp)
 
-   })
+    if(input$forcefield %in% c("anm", "pfanm"))
+      modes <<- nma(pdb, ff=input$forcefield, cutoff=input$cutoff,
+                    mass=input$mass, temp=temp)
+    
+    return(modes)
+  })
+  
+  ######################
+  ##- DCCM calculation #
+  ######################
+  calcDCCM <- reactive({
+    message("calculating dccm")
+    modes <- calcModes()
+    cij <- dccm(modes)
+    return(cij)
+  })
+  
+  output$modeSummary <- renderPrint({
+    input$nmaaction
+    
+    invisible(capture.output( modes <- calcModes()))
+    print(modes)
+  })
 
-    calcDCCM <- reactive({
-        modes <- calcModes()
-        cij <- dccm(modes)
-        return(cij)
-    })
-
-    output$fluctPlot <- renderPlot({
-        pdb <- checkPDB()
-        modes <- calcModes()
-
-        x <- modes$fluctuations
-        main <- paste("NMA on PDB id", input$pdbid)
-
-        # draw fluctuations
-        par(mfrow=c(2,1))
-        plot.bio3d(x, sse=pdb, main=main)
-
+  ##########################
+  ##- Fluctuation plot     #
+  ##########################
+  output$fluct_plot <- renderPlot({
+    fluct_plot2()
+  })
+  
+  fluct_plot2<- reactive({
+    fluct_plot1()
+  })
+  
+  fluct_plot1 <- function() {
+    pdb <- finalPDB()
+    modes <- calcModes()
+    x <- modes$fluctuations
+    
+    if(input$fluxs1 & input$fluxs3) { 
+      main <- paste("NMA derived fluctuations for PDB id", input$pdbid)
+      plot.bio3d(x, sse=pdb, main=main, resno=pdb,
+                 col=input$col1, type=input$typ1,
+                 pch=input$pch1, lty=input$lty1, cex=input$cex1, lwd=input$lwd1)
+    }
+    if(input$fluxs2) {
+      if(input$fluxs3) {
+        par(new=TRUE)
+        plot.bio3d(pdb$atom$b[ pdb$calpha ], resno=pdb, axes=F, xlab="",ylab="",
+             pch=input$pch2, col=input$col2, typ=input$typ2, lty=input$lty2, lwd=input$lwd2)
+        axis(4, col=input$col2)
+      }
+      else {
         main <- paste("B-factors for PDB id", input$pdbid)
-        plot.bio3d(pdb$atom$b[ pdb$calpha ], sse=pdb, main=main)
+        plot.bio3d(pdb$atom$b[ pdb$calpha ], sse=pdb, main=main, resno=pdb,
+             pch=input$pch2, col=input$col2, typ=input$typ2, lty=input$lty2, cex=input$cex2,
+                   lwd=input$lwd2)
+      }
+    }
+  }
+  
+  output$plot1pdf = downloadHandler(
+    filename = 'fluct.pdf',
+    content = function(file) {
+      pdf(file, w=10, h=6)
+      fluct_plot1()
+      dev.off()
+    })
+  
+  
+  ##################
+  ##- DCCM plot    #
+  ##################
+  output$dccm_plot <- renderPlot({
+    dccm_plot2()
+  })
+  
+  dccm_plot2 <- reactive({
+    dccm_plot1()
   })
 
+  
+  dccm_plot1 <- function() {
+    message("running plot function")
+    
+    if(input$cijs) {
+    
+      if(input$contourplot==FALSE) {
+        contour <- FALSE
+        cols <- bwr.colors(20)
+        at <- seq(-1, 1, 0.1)
+      }
+      else {
+        contour <- TRUE
+        cols <- NULL
+        at <- c(-1, -0.75, -0.5,  -0.25, 0.25, 0.5, 0.75, 1)
+      }
+      
+      if(input$sse)
+        sse <- pdb
+      else
+        sse <- NULL
+      
+    progress <- shiny::Progress$new(session, min=1, max=10)
+    on.exit(progress$close())
+    
+    progress$set(message = 'Calculation in progress',
+                 detail = 'This may take a moment...')
+    
+    for (i in 1:5) {
+      progress$set(value = i)
+      Sys.sleep(0.1)
+    }
+    
+    
+    main <- paste("DCCM, PDB id", input$pdbid)
+    cij <- calcDCCM()
+    
+    for (i in 6:10) {
+      progress$set(value = i)
+      Sys.sleep(0.1)
+    }
+    
+    
+    plot(cij, sse=sse, main=main,
+         contour=contour,
+         col.regions=cols, colorkey=input$colorkey, at=at)
+  }
+  }
 
-
-
-    output$dccmPlot <- renderPlot({
-        pdb <- checkPDB()
-
-        if(input$style=="Red-blue") {
-            contour <- FALSE
-            cols <- bwr.colors(20)
-            at <- seq(-1, 1, 0.1)
-        }
-        else {
-            contour <- TRUE
-            cols <- NULL
-            at <- c(-1, -0.75, -0.5,  -0.25, 0.25, 0.5, 0.75, 1)
-        }
-
-        if(input$sse)
-            sse <- pdb
-        else
-            sse <- NULL
-
-        progress <- shiny::Progress$new(session, min=1, max=10)
-        on.exit(progress$close())
-
-        progress$set(message = 'Calculation in progress',
-                     detail = 'This may take a while...')
-
-        for (i in 1:5) {
-            progress$set(value = i)
-            Sys.sleep(0.1)
-        }
-
-
-        main <- paste("DCCM, PDB id", input$pdbid)
-        cij <- calcDCCM()
-
-        for (i in 6:10) {
-            progress$set(value = i)
-            Sys.sleep(0.1)
-        }
-
-
-        plot(cij, sse=sse, main=main, contour=contour,
-             col.regions=cols, colorkey=input$colorkey, at=at)
-
-  })
-
-    output$pdbSummary <- renderPrint({
-        invisible(capture.output( pdb <- checkPDB()))
-        print(pdb)
+  
+  output$plot2pdf = downloadHandler(
+    filename = 'dccm.pdf',
+    content = function(file) {
+      pdf(file, w=10, h=6)
+      dccm_plot1()
+      dev.off()
     })
+  
+    
+  
 
-    output$modeSummary <- renderPrint({
-        invisible(capture.output( modes <- calcModes()))
-        print(modes)
-    })
-
+  
+  
 })
-
--- shiny_devel/nma-app/ui.R
+++ shiny_devel/nma-app/ui.R
@@ -1,82 +1,204 @@
-
 library(shiny)
-
-# Define UI for application that draws a histogram
-shinyUI(navbarPage("Bio3D NMA",
-
-                   tabPanel("PDB",
-                            sidebarLayout(
-
-                                sidebarPanel(
-                                    textInput("pdbid", "Ender a PDB code:", "1hel"),
-
-                                    textInput("chain", "Chain ID:", "A"),
-
-                                    submitButton("Submit")
-                                    ),
+library(bio3d)
+library(shinyRGL)
 
 
-                                mainPanel(
-                                    h4("Summary"),
-                                    verbatimTextOutput("pdbSummary")
-                                    )
-                                )
-                            ),
+## Could open some saved results as an RData file here for first time display 
+## Calculations would then only run once single SUBMIT button was pressed.
 
-                   tabPanel("NMA",
-                            sidebarLayout(
-                                sidebarPanel(
-                                    selectInput("forcefield", "Choose a forcefield:",
-                                                choices = c("calpha", "anm", "pfanm", "sdenm")),
+shinyUI(fluidPage(
 
-                                    sliderInput("cutoff",
-                                                "Cutoff value:",
-                                                min = 6,
-                                                max = 50,
-                                                value = 10),
+  title = "Bio3D NMA",
 
-                                    radioButtons("mass", "Mass-weighting:",
-                                                 c("Yes", "Nope")),
+  h1("Bio3D NMA"),  
+  h2("Input"),  
+  hr(),
 
-                                    sliderInput("temp",
-                                                "Temperature scaling:",
-                                                min = 0,
-                                                max = 350,
-                                                value = 300),
+  fluidRow(
+    column(4,
+      wellPanel(
+        h4("1. PDB Input Selection"),
+        tags$hr(),
 
-                                    submitButton("Submit")
-                                    ),
+        ##-PDB input (moved to server.R)
+        uiOutput('resetable_pdb_input'),
+        #textInput("pdbid", label="Enter RCSB PDB code/ID:", value = "4Q21"),
 
-                                mainPanel(
-                                    h4("Summary"),
-                                    verbatimTextOutput("modeSummary")
-                                    )
-                                )
-                            ),
-
-                   tabPanel("Fluctuations",
-                            plotOutput("fluctPlot")
-                            ),
-
-                     tabPanel("DCCM",
-                              sidebarLayout(
-                                sidebarPanel(
-                                    radioButtons("style", "Style:",
-                                                 c("Cyan", "Red-blue")),
-
-                                    checkboxInput('colorkey', 'Color key', TRUE),
-
-                                    checkboxInput('sse', 'Show SSE', TRUE),
-
-                                    submitButton("Submit")
-                                    ),
-
-                                  mainPanel(
-                                      plotOutput("dccmPlot")
-                                      )
-                                  )
-                              )
-                   )
+        ##- Chain selection
+        h5("Detected chain IDs:"),
+        verbatimTextOutput("chains1"),
+        
+        checkboxInput("limit", "Limit calculation to a subset of chains?"),
+        helpText("Note: Use this option to exclude particular chains form further consideration."),
+        
+        conditionalPanel(
+          condition = "input.limit == true",
+          uiOutput("chains2"),
+          helpText("Note: Only selected chains will be analyzed.")
         )
 
+        ##-TODO Have this panel auto-update only!
+        #,actionButton("pdbaction", "Fetch PDB")
+        #,submitButton("Submit")
+        ,actionButton("reset_pdb_input", "Reset PDB input")
+      )
+    ),
 
+    column(4,
+      wellPanel(
+        h4("2. NMA Calculation"),
+        tags$hr(),
+
+        uiOutput('resetable_nma_input'),
+        
+        helpText("Note: Some help text here. "),
+ 
+        actionButton("reset_nma_input", "Reset NMA inputs")
+        ##submitButton("Submit") - will effect first PDB input panel also
+      )
+    ),
+    
+    column(4,
+      wellPanel(
+        h4("3. Result Visualization"),
+        tags$hr(),
+        checkboxInput('fluxs1', 'Fluctuations', value=TRUE),
+        #checkboxInput('fluxs2', 'Fluctuations with B-factors', value=FALSE),
+        checkboxInput('cijs', 'Correlations', value=FALSE),
+        checkboxInput('mktrj', 'Mode Displacements Trajectory', value=FALSE),
+        checkboxInput('log', 'Calculation Summary Log', value=TRUE)
+      )
+    )
+  ),
+
+
+
+  #########################
+  ##-- Results Section --##
+  #########################
+  hr(),
+  h2("Results"),
+
+  ##-A. Fluctuations Panel
+  conditionalPanel(
+    condition = "input.fluxs1 == true",
+
+    h3("Fluctuations"),
+    fluidRow(
+      plotOutput("fluct_plot"),
+      column(3,
+             checkboxInput('show_options', 'More options', value=FALSE)
+             ),
+      column(3,
+             checkboxInput('fluxs2', 'Show B-factors', value=FALSE)
+             ),
+      column(3),
+      column(3,
+             downloadButton('plot1pdf', "Download Plot PDF")
+             )
+      ),
+
+    conditionalPanel(
+      condition = "input.show_options == true",
+      fluidRow(
+        column(3,
+               h4("Plot options"),
+               checkboxInput('fluxs3', 'Show', value=TRUE),
+               selectInput("typ1", "Type:",
+                           c("hist" = "h",
+                             "lines" = "l",
+                             "points" = "p",
+                             "both" = "b")),
+               sliderInput("lty1", "Line type:",
+                           min = 1, max = 6, value = 1),
+               sliderInput("lwd1", "Line width:",
+                           min = 0.1, max = 2, value = 1, step=0.1)
+               ),
+
+        column(3,
+               h4("Plot options"),
+               sliderInput("pch1", "Point type:",
+                           min = 1, max = 25, value = 1),
+               sliderInput("cex1", "Point size:",
+                           min = 0.1, max = 2, value = 1, step=0.1),
+               sliderInput("col1", "Color:",
+                           min = 1, max = 8, value = 1)
+               ),
+        
+        column(3,
+               h4("B-factors"),
+               checkboxInput('fluxs2', 'Show B-factors', value=FALSE),
+               selectInput("typ2", "Type:",
+                           c("lines" = "l",
+                             "points" = "p",
+                             "both" = "b",
+                             "hist" = "h")),
+               
+               sliderInput("lty2", "Line type:",
+                           min = 1, max = 6, value = 1),
+               sliderInput("lwd2", "Line width:",
+                           min = 0.1, max = 2, value = 1, step=0.1)
+               ),
+
+        column(3,
+               h4("B-factors"),
+               sliderInput("pch2", "Point type:",
+                           min = 1, max = 25, value = 1),
+               sliderInput("cex2", "Point size:",
+                           min = 0.1, max = 2, value = 1, step=0.1),
+               sliderInput("col2", "Color:",
+                           min = 1, max = 8, value = 4)
+               )
+        ) ## end fluid row
+      ) ## end conditional panel
+    
+   
+    ),
+  
+  br(),br(),
+
+
+
+
+
+  ##-C. DCCM Panel
+  conditionalPanel(
+    condition = "input.cijs == true",
+    h3("Correlations"),
+    fluidRow(
+      plotOutput("dccm_plot")
+      ),
+    fluidRow(
+      column(3,
+             checkboxInput('contourplot', 'Contourplot', value=TRUE)
+             ),
+      column(3,
+             checkboxInput('sse', 'Show SSE', value=TRUE)
+             ),
+      column(3,
+             checkboxInput('colorkey', 'Colorkey', value=TRUE)
+             ),
+      column(3,
+             downloadButton('plot2pdf', "Download Plot PDF")
+             )
+      )
+    ),
+  br(),br(),
+
+  
+
+  hr(),
+  ##-D. Log Report Panel
+  conditionalPanel(
+    condition = "input.log == true",
+    column(6,
+           h4("PDB Summary"),
+           verbatimTextOutput("pdbSummary")
+           ),
+    column(6,
+           h4("Calculation Summary Log"),
+           verbatimTextOutput("modeSummary")
+           )
+    
+    )
+))
