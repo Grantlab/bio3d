fca77261201b8e346302f0e2d5972c96450ae292
lars Skjærven <larsss@gmail.com>
2016-08-18 15:30:28 +0200

    OTHER: filter.cmap- can take a list of contact maps, not only an array. minor updates to checking and documentation

--- ver_devel/bio3d/R/filter.cmap.R
+++ ver_devel/bio3d/R/filter.cmap.R
@@ -1,20 +1,35 @@
-filter.cmap <- function(cm, cutoff.sims = dim(cm)[3]) {
+filter.cmap <- function(cm, cutoff.sims = NULL) {
 
+  ## Convert list to array  
+  if(is.list(cm)) {
+      dims <- dim(cm[[1]])
+      if(length(dims) != 2)
+          stop("Input 'cm' should be a NxNxS 3d array or a list of NxN matrices,
+      where N is the number of atoms and S is the number of simulations")
+      
+      if(length(unique(c(sapply(cm, dim)))) != 1)
+          stop("Matrices in provided list have unequal dimensions")
+      
+      cm <- array(unlist(cm), dim = c(nrow(cm[[1]]), ncol(cm[[1]]), length(cm)))
+  }
+    
   ## Check input
-  if (length(dim(cm)) != 3) {
-    stop("Input 'cm' should be a NxNxS 3d array,\n 
+  dims <- dim(cm)
+  if(length(dims) != 3) {
+      stop("Input 'cm' should be a NxNxS 3d array or a list of NxN matrices,
       where N is the number of atoms and S is the number of simulations")
   }
 
-  if (!(is.numeric(cutoff.sims) && (cutoff.sims <= dim(cm)[3]))) {
-    stop("Input 'cutoff.sims' should a number between 1 and dim(cm)[3],\n 
-      i.e. the number of simulations upon which filtering is based")
-  }
+  if(dims[3L] < 2)
+      stop("2 or more contact maps required")
+  
+  if(is.null(cutoff.sims))
+      cutoff.sims <- dims[3L]
 
-  if ((is.numeric(cutoff.sims)) <= 0) {
-    print("WARNING!! cutoff.sims' should a number between 1 and dim(cm)[3],\n 
-      i.e. the number of simulations upon which filtering is based")
-  }
+    if(!is.numeric(cutoff.sims) | cutoff.sims > dims[3L] | cutoff.sims < 1) {
+        stop(paste("Input 'cutoff.sims' should be a number between 1 and", dims[3L], "\n  ",
+                   "(i.e. the number of simulations upon which filtering is based)"))
+    }
 
   ## Sum across simulations and filter by cutoff.sims de
   cm.sum <- apply(cm, c(1:2), sum, na.rm = TRUE)
