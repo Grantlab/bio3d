1f69548cb4a8cf46f3b647e60e27cbfecdaa218c
lars Skjærven <larsss@gmail.com>
2016-09-29 11:53:48 +0200

    OTHER: minor update of plot.enma.R after prev update of plotb3 by XQ. fixed issue with NAs in resno and SSE

--- ver_devel/bio3d/R/plot.enma.R
+++ ver_devel/bio3d/R/plot.enma.R
@@ -6,8 +6,44 @@
 
     if(!(inherits(x, "enma")))
         stop("provide a enma object as obtained from 'nma.pdbs'")
-    yval <- x$fluctuations
 
+      yval <- x$fluctuations
+
+
+      ## SSE information
+      sse.aln <- NULL
+      dots <- list(...)
+
+      ## reference structure for SSE and resno
+      if("ind" %in% names(dots)) {
+          ref.ind <- dots[["ind"]]
+          dots[["ind"]] <- NULL
+
+          ## Note - plot.fluct use plotb3(yval[1,]) for base plotting
+          ## Lines 54-61 checks for NAs in yval[1,] to trim SSE
+          ## SSE must therefore correspond to yval[1,]
+          ## ref.ind != 1 might give wrong sse annotation in plot
+          
+          warning("reference structure can not be set")
+          ref.ind <- 1
+      }
+      else {
+          ref.ind <- 1
+      }
+
+      ## use first non-NA in col as ref.ind
+      if("col" %in% names(dots)) {
+          col <- dots[["col"]]
+
+          if(length(col) != nrow(yval))
+              stop("length of col doesn't match dimension of x")
+          
+          if(any(is.na(col))) {
+              ref.ind <- which(!is.na(col))[1]
+          }
+      }
+
+  
     ## check for gaps
     gaps <- gap.inspect(yval)
     if(any(gaps$col>0))
@@ -44,12 +80,12 @@
             pdbs <- trim(pdbs, col.inds=gaps.pdbs$f.inds)
     }
 
-    if(!is.null(pdbs)) {
-        resno <- pdbs$resno[1, ]
+      if(!is.null(pdbs)) {
+        resno <- pdbs$resno[ref.ind, ]
 
         if(is.null(xlab)) {
             xlab <- paste0('Residue number (reference PDB: ',
-                           basename.pdb(pdbs$id[1]), ')')
+                           basename.pdb(pdbs$id[ref.ind]), ')')
         }
     }
     else {
@@ -59,29 +95,25 @@
             xlab <- "Alignment Position"
     }
 
-    ## SSE information
-    sse.aln <- NULL
-    dots <- list(...)
+      
     if( "sse" %in% names(dots) ) {
-      sse.aln <- dots$sse
-      dots$sse <- NULL
+        sse.aln <- dots$sse
+        dots$sse <- NULL
     }
-
+      
     if(!is.null(pdbs) & is.null(sse.aln)) {
-        rm.gaps <- TRUE ## required by plotb3, see lines 70-76
-        sse.aln <- pdbs2sse(pdbs, ind=1, rm.gaps=rm.gaps, resno=TRUE)
-        print(sse.aln)
+        sse.aln <- pdbs2sse(pdbs, ind=ref.ind, rm.gaps=rm.gaps, resno=TRUE)
+        sse.aln$sse[ is.na(yval[ref.ind,]) ] <- NA
+        ## see lines 54-61 in plotb3.R
     }
 
     if( "rm.gaps" %in% names(dots) ) {
         warning("'rm.gaps=TRUE' might result in incorrect SSE annotation")
     }
 
-    resno2 = resno[!is.na(resno)] ## req by plotb3 line 25
-
     ## Plot fluctuations
     do.call('plot.fluct', c(list(x=yval,
-                                 resno=resno2, sse=sse.aln,
+                                 resno=resno, sse=sse.aln,
                                  xlab=xlab, ylab=ylab),
                             dots))
 
