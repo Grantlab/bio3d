21e44d470875fc33a57b20bca28a5f09d8ef3ef1
Xin-Qiu Yao <xinqiu.yao@gmail.com>
2019-11-25 17:56:17 -0500

    OTHER: bugfix in building the image for html docs

--- ver_devel/util/run_generate_html_docker.sh
+++ ver_devel/util/run_generate_html_docker.sh
@@ -5,5 +5,8 @@ mkdir /tmp/bio3d-html
 
 docker run -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp/.docker.xauth:/tmp/.docker.xauth -e XAUTHORITY=/tmp/.docker.xauth -e DISPLAY=:0 --rm -it --volume=/tmp/bio3d-html:/tmp/bio3d --workdir=/bio3d xinqyao/bio3d-html
 
-mv /tmp/bio3d-html/* sandbox/
+fnm=$( basename /tmp/bio3d-html/* )
+cp -r /tmp/bio3d-html/$fnm sandbox/
+rm -f html
+ln -s sandbox/$fnm html
 rm -rf /tmp/bio3d-html
